1d6bec27547ab7d1238fd7c84e5b5d5b62f128c9
Wei_NaChuan <weinachuan1@huawei.com>
2025-08-26 09:11:30 +0800

    add gmmSwigluQuant example
    Co-authored-by: Wei_NaChuan<weinachuan1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge add-gmmswigluquant-example into master
    
    add gmmSwigluQuant example
    
    Created-by: Wei_NaChuan
    Commit-by: Wei_NaChuan
    Merged-by: ascend-robot
    Description: add gmmSwigluQuant example
    
    See merge request: cann/ops-transformer-dev!19

---
+++ examples/gmm/grouped_matmul_swiglu_quant/test_grouped_matmul_swiglu_quant.cpp
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2024-2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_grouped_matmul_swiglu_quant.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <fcntl.h>
+#include <filesystem>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "acl/acl.h"
+#include "grouped_matmul_swiglu_quant_utils.h"
+#include "aclnnop/aclnn_grouped_matmul_swiglu_quant.h"
+namespace fs = std::filesystem;
+using namespace GroupedMatmulSwigluQuantUtils;
+
+int main()
+{
+    // 1. （固定写法）device/stream初始化，参考acl API手册
+    int32_t deviceId = 0;
+    aclrtStream stream;
+    auto ret = Init(deviceId, &stream);
+    // check根据自己的需要处理
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("Init acl failed. ERROR: %d\n", ret); return ret);
+
+    // 2. 构造输入与输出，需要根据API的接口自定义构造
+    int64_t E = 4;
+    int64_t M = 128;
+    int64_t K = 512;
+    int64_t N = 256;
+    std::vector<int64_t> xShape = {M, K};
+    std::vector<int64_t> weightShape = {E, N / 32, K / 16, 16, 32};
+    std::vector<int64_t> weightScaleShape = {E, N};
+    std::vector<int64_t> xScaleShape = {M};
+    std::vector<int64_t> groupListShape = {E};
+    std::vector<int64_t> outputShape = {M, N / 2};
+    std::vector<int64_t> outputScaleShape = {M};
+
+    void *xDeviceAddr = nullptr;
+    void *weightDeviceAddr = nullptr;
+    void *weightScaleDeviceAddr = nullptr;
+    void *xScaleDeviceAddr = nullptr;
+    void *groupListDeviceAddr = nullptr;
+    void *outputDeviceAddr = nullptr;
+    void *outputScaleDeviceAddr = nullptr;
+
+    aclTensor *x = nullptr;
+    aclTensor *weight = nullptr;
+    aclTensor *weightScale = nullptr;
+    aclTensor *xScale = nullptr;
+    aclTensor *groupList = nullptr;
+    aclTensor *output = nullptr;
+    aclTensor *outputScale = nullptr;
+
+    std::vector<int8_t> xHostData(M * K, 0);
+    std::vector<int8_t> weightHostData(E * N * K, 0);
+    std::vector<float> weightScaleHostData(E * N, 0);
+    std::vector<float> xScaleHostData(M, 0);
+    std::vector<int64_t> groupListHostData(E, 0);
+    std::vector<int8_t> outputHostData(M * N / 2, 0);
+    std::vector<float> outputScaleHostData(M, 0);
+
+    std::filesystem::path currentPath = std::filesystem::current_path();
+    fs::path fileXBin = "x.bin";
+    fs::path fileWeightBin = "weight.bin";
+    fs::path fileWeightScaleBin = "weightScale.bin";
+    fs::path fileXScaleBin = "xScale.bin";
+    fs::path fileGroupListBin = "groupList.bin";
+    fs::path fileOutputBin = "output.bin";
+    fs::path fileOutputScaleBin = "outputScale.bin";
+
+    ReadFile(currentPath / fileXBin, xShape, xHostData);
+    ReadFile(currentPath / fileWeightBin, weightShape, weightHostData);
+    ReadFile(currentPath / fileWeightScaleBin, weightScaleShape, weightScaleHostData);
+    ReadFile(currentPath / fileWeightScaleBin, xScaleShape, xScaleHostData);
+    ReadFile(currentPath / fileGroupListBin, groupListShape, groupListHostData);
+
+    // 创建x aclTensor
+    ret = CreateAclTensor(xHostData, xShape, &xDeviceAddr, aclDataType::ACL_INT8, aclFormat::ACL_FORMAT_ND, &x);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建weight aclTensor
+    ret = CreateAclTensor(weightHostData, weightShape, &weightDeviceAddr, aclDataType::ACL_INT8,
+                          aclFormat::ACL_FORMAT_FRACTAL_NZ, &weight);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建weightScale aclTensor
+    ret = CreateAclTensor(weightScaleHostData, weightScaleShape, &weightScaleDeviceAddr, aclDataType::ACL_FLOAT,
+                          aclFormat::ACL_FORMAT_ND, &weightScale);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建xScale aclTensor
+    ret = CreateAclTensor(xScaleHostData, xScaleShape, &xScaleDeviceAddr, aclDataType::ACL_FLOAT,
+                          aclFormat::ACL_FORMAT_ND, &xScale);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建groupList aclTensor
+    ret = CreateAclTensor(groupListHostData, groupListShape, &groupListDeviceAddr, aclDataType::ACL_INT64,
+                          aclFormat::ACL_FORMAT_ND, &groupList);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建output aclTensor
+    ret = CreateAclTensor(outputHostData, outputShape, &outputDeviceAddr, aclDataType::ACL_INT8,
+                          aclFormat::ACL_FORMAT_ND, &output);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    // 创建outputScale aclTensor
+    ret = CreateAclTensor(outputScaleHostData, outputScaleShape, &outputScaleDeviceAddr, aclDataType::ACL_FLOAT,
+                          aclFormat::ACL_FORMAT_ND, &outputScale);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    uint64_t workspaceSize = 0;
+    aclOpExecutor *executor;
+
+    // 3. 调用CANN算子库API
+    // 调用aclnnGroupedMatmulSwigluQuant第一段接口
+    ret = aclnnGroupedMatmulSwigluQuantGetWorkspaceSize(x, weight, nullptr, nullptr, weightScale, xScale, groupList,
+                                                        output, outputScale, nullptr, &workspaceSize, &executor);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGroupedMatmulSwigluQuantGetWorkspaceSize failed. ERROR: %d\n", ret);
+              return ret);
+    // 根据第一段接口计算出的workspaceSize申请device内存
+    void *workspaceAddr = nullptr;
+    if (workspaceSize > 0) {
+        ret = aclrtMalloc(&workspaceAddr, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("allocate workspace failed. ERROR: %d\n", ret); return ret);
+    }
+    // 调用aclnnGroupedMatmulSwigluQuant第二段接口
+    ret = aclnnGroupedMatmulSwigluQuant(workspaceAddr, workspaceSize, executor, stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclnnGroupedMatmulSwigluQuant failed. ERROR: %d\n", ret); return ret);
+
+    // 4. （固定写法）同步等待任务执行结束
+    ret = aclrtSynchronizeStream(stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("aclrtSynchronizeStream failed. ERROR: %d\n", ret); return ret);
+
+    // 5. 获取输出的值，将Device侧内存上的结果拷贝至Host侧，需要根据具体API的接口定义修改
+    auto size = GetShapeSize(outputShape);
+    std::vector<int8_t> out1Data(size, 0);
+    ret = aclrtMemcpy(out1Data.data(), out1Data.size() * sizeof(out1Data[0]), outputDeviceAddr,
+                      size * sizeof(out1Data[0]), ACL_MEMCPY_DEVICE_TO_HOST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy result from device to host failed. ERROR: %d\n", ret); return ret);
+    WriteFile(currentPath / fileOutputBin, size, out1Data);
+
+    size = GetShapeSize(outputScaleShape);
+    std::vector<float> out2Data(size, 0);
+    ret = aclrtMemcpy(out2Data.data(), out2Data.size() * sizeof(out2Data[0]), outputScaleDeviceAddr,
+                      size * sizeof(out2Data[0]), ACL_MEMCPY_DEVICE_TO_HOST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("copy result from device to host failed. ERROR: %d\n", ret); return ret);
+    WriteFile(currentPath / fileOutputScaleBin, size, out2Data);
+    // 6. 释放aclTensor和aclScalar，需要根据具体API的接口定义修改
+    aclDestroyTensor(x);
+    aclDestroyTensor(weight);
+    aclDestroyTensor(weightScale);
+    aclDestroyTensor(xScale);
+    aclDestroyTensor(groupList);
+    aclDestroyTensor(output);
+    aclDestroyTensor(outputScale);
+
+    // 7. 释放device资源，需要根据具体API的接口定义修改
+    aclrtFree(xDeviceAddr);
+    aclrtFree(weightDeviceAddr);
+    aclrtFree(weightScaleDeviceAddr);
+    aclrtFree(xScaleDeviceAddr);
+    aclrtFree(groupListDeviceAddr);
+    aclrtFree(outputDeviceAddr);
+    aclrtFree(outputScaleDeviceAddr);
+    if (workspaceSize > 0) {
+        aclrtFree(workspaceAddr);
+    }
+    aclrtDestroyStream(stream);
+    aclrtResetDevice(deviceId);
+    aclFinalize();
+    return 0;
+}
