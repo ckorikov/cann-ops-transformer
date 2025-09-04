49bd89e14c9cbf34e75270b3977c00810f0506ee
huang-chuhong <huangchuhong1@h-partners.com>
2025-09-04 18:57:50 +0800

    transformer cmake修改
    Co-authored-by: huangchuhong<huangchuhong1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    transformer cmake修改
    
    Created-by: huang-chuhong
    Commit-by: huangchuhong
    Merged-by: ascend-robot
    Description: transformer cmake修改
    
    See merge request: cann/ops-transformer-dev!52

--- cmake/func.cmake
+++ cmake/func.cmake
@@ -45,13 +45,9 @@ function(op_add_subdirectory OP_LIST OP_DIR_LIST)
 
     file(GLOB OP_HOST_CMAKE_FILES 
     "${CMAKE_CURRENT_SOURCE_DIR}/gmm/**/op_host/CMakeLists.txt"
-    "${CMAKE_CURRENT_SOURCE_DIR}/moe/**/op_host/CMakeLists.txt"
     "${CMAKE_CURRENT_SOURCE_DIR}/attention/**/op_host/CMakeLists.txt"
-    "${CMAKE_CURRENT_SOURCE_DIR}/mc2/**/op_host/CMakeLists.txt"
     "${CMAKE_CURRENT_SOURCE_DIR}/gmm/**/CMakeLists.txt"
-    "${CMAKE_CURRENT_SOURCE_DIR}/moe/**/CMakeLists.txt"
-    "${CMAKE_CURRENT_SOURCE_DIR}/attention/**/CMakeLists.txt"
-    "${CMAKE_CURRENT_SOURCE_DIR}/mc2/**/CMakeLists.txt")
+    "${CMAKE_CURRENT_SOURCE_DIR}/attention/**/CMakeLists.txt")
 
     foreach(OP_CMAKE_FILE ${OP_HOST_CMAKE_FILES})
         if ("${OP_CMAKE_FILE}" MATCHES "op_host")
