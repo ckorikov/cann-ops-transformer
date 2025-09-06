3dd2a7ed4d1ffa48652871558b486bcd05971f80
ascend-robot <zhongyuanke@huawei.com>
2025-09-06 17:25:45 +0800

    整仓example
    Co-authored-by: feng-zixiao-1019<fengzixiao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    整仓example
    
    Created-by: fengzixiao
    Commit-by: feng-zixiao-1019
    Merged-by: ascend-robot
    Description: 整仓example
    
    See merge request: cann/ops-transformer-dev!55

--- gmm/grouped_matmul_swiglu_quant/example/grouped_matmul_swiglu_quant_output_result.py
+++ gmm/grouped_matmul_swiglu_quant/example/grouped_matmul_swiglu_quant_output_result.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# coding: utf-8
 # Copyright (c) 2024-2025 Huawei Technologies Co., Ltd.
 # This file is a part of the CANN Open Software.
 # Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
@@ -7,4 +9,19 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # ======================================================================================================================
 
-add_subdirectory(grouped_matmul_swiglu_quant)
+import numpy as np
+import sys
+import os
+
+if __name__ == '__main__':
+    test_case = sys.argv[1]
+    print_head = "====================== Show sample " + test_case + " result start ================="
+    print(print_head)
+    file1 = 'output.bin'
+    file2 = 'outputScale.bin'
+    if not os.path.isfile(file1) or not os.path.isfile(file2):
+        raise RuntimeError(f"Invalid case name:", test_case)
+    output = np.fromfile(file1, dtype=np.int8)
+    print(f"{test_case} output: ", output)
+    outputScale = np.fromfile(file2, dtype=np.float32)
+    print(f"{test_case} outputScale: ", outputScale)
