deaa2c474602a59a71eb85a6a118b4043e4b5bd8
ascend-robot <zhongyuanke@huawei.com>
2025-09-11 22:30:31 +0800

    add_gmmadd
    Co-authored-by: 张硕累<zhangshuolei@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge add_gmm_add into master
    
    add_gmmadd
    
    Created-by: zhangshuoleiokok
    Commit-by: 张硕累
    Merged-by: ascend-robot
    Description: add_gmmadd
    
    See merge request: cann/ops-transformer-dev!70

---
+++ gmm/grouped_matmul_add/tests/ut/op_host/test_grouped_matmul_add_tiling.cpp
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+
+/*!
+ * \file test_grouped_matmul_add.cpp
+ * \brief
+ */
+
+#include <iostream>
+#include <vector>
+
+#include <gtest/gtest.h>
+#include "op_log.h"
+
+#include "runtime2_util.h"
+#include "kernel_run_context_facker.h"
+#include "experiment_ops.h"
+#include "array_ops.h"
+#include "nn_other.h"
+#include "op_tiling/op_tiling_util.h"
+#include "common/utils/ut_op_util.h"
+#include "common_unittest.h"
+#include "transformer/grouped_matmul_add/op_host/op_tiling/grouped_matmul_add_tiling.h"
+#include "exe_graph/runtime/storage_format.h"
+#include "exe_graph/runtime/storage_shape.h"
+#include "test_cube_util.h"
+
+using namespace ut_util;
+using namespace std;
+using namespace ge;
+
+class GroupedMatmulAddTiling : public testing::Test
+{
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "GroupedMatmulAddTiling SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "GroupedMatmulAddTiling TearDown" << std::endl;
+    }
+};
+
+TEST_F(GroupedMatmulAddTiling, test_tiling_bf16)
+{
+    std::string op_type("GroupedMatmulAdd");
+    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str()), nullptr);
+    auto tiling_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling;
+    auto tiling_parse_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling_parse;
+
+    string compile_info_string = R"({
+                                        "hardware_info": {
+                                            "BT_SIZE": 1024,
+                                            "load3d_constraints": "0",
+                                            "Intrinsic_fix_pipe_l0c2out": true,
+                                            "Intrinsic_data_move_l12ub": false,
+                                            "Intrinsic_data_move_l0c2ub": false,
+                                            "Intrinsic_data_move_out2l1_nd2nz": true,
+                                            "UB_SIZE": 196608,
+                                            "L2_SIZE": 33554432,
+                                            "L1_SIZE": 524288,
+                                            "L0A_SIZE": 65536,
+                                            "L0B_SIZE": 65536,
+                                            "L0C_SIZE": 131072,
+                                            "CORE_NUM": 20
+                                        }
+                                    })";
+    map<string, string> soc_infos;
+    map<string, string> aicore_spec;
+    map<string, string> intrinsics;
+    GetPlatFormInfos(compile_info_string.c_str(), soc_infos, aicore_spec, intrinsics);
+
+    // platform info
+    fe::PlatFormInfos platform_info;
+    platform_info.Init();
+    // compile info
+    struct GroupedMatMulAllReduceCompileInfo {
+    } compile_info;
+
+    // tilingParseFunc simulate
+    auto kernel_holder =
+        gert::KernelRunContextFaker()
+            .KernelIONum(4, 1)
+            .Inputs({const_cast<char*>(compile_info_string.c_str()), reinterpret_cast<void*>(&platform_info)})
+            .Outputs({&compile_info})
+            .Build();
+
+    // tilingFunc simulate
+    auto param = gert::TilingData::CreateCap(12016);
+    ASSERT_NE(param, nullptr);
+    auto workspace_size_holer = gert::ContinuousVector::Create<size_t>(4096);
+    auto ws_size = reinterpret_cast<gert::ContinuousVector*>(workspace_size_holer.get());
+    gert::StorageShape xShape = {{1280, 345}, {1280, 345}};
+    gert::StorageShape wShape = {{1280, 567}, {1280, 567}};
+    gert::StorageShape groupListShape = {{2}, {2}};
+    gert::StorageShape yShape = {{690, 567}, {690, 567}};
+    std::vector<std::pair<size_t, std::unique_ptr<uint8_t[]>>> const_tensors;
+
+    auto holder = gert::TilingContextFaker()
+                      .NodeIoNum(4, 1)
+                      .IrInstanceNum({1, 1, 1, 1})
+                      .InputShapes({&xShape, &wShape, &groupListShape, &yShape})
+                      .OutputShapes({&yShape})
+                      .NodeAttrs(
+                          {{"transpose_weight", ge::AnyValue::CreateFrom<bool>(false)},
+                           {"transpose_x", ge::AnyValue::CreateFrom<bool>(true)}})
+                      .CompileInfo(&compile_info)
+                      .PlatformInfo(reinterpret_cast<char*>(&platform_info))
+                      .NodeInputTd(0, ge::DT_BF16, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(1, ge::DT_BF16, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(2, ge::DT_INT64, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(3, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeOutputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .TilingData(param.get())
+                      .Workspace(ws_size)
+                      .SetOpType(op_type)
+                      .Build();
+
+    gert::TilingContext* tiling_context = holder.GetContext<gert::TilingContext>();
+    ASSERT_NE(tiling_context->GetPlatformInfo(), nullptr);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap", intrinsics);
+
+    EXPECT_EQ(tiling_func(tiling_context), ge::GRAPH_SUCCESS);
+
+    // to check tiling result
+    auto tiling_key = tiling_context->GetTilingKey();
+    ASSERT_EQ(tiling_key, 1);
+}
+
+TEST_F(GroupedMatmulAddTiling, test_tiling_fp16)
+{
+    std::string op_type("GroupedMatmulAdd");
+    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str()), nullptr);
+    auto tiling_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling;
+    auto tiling_parse_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling_parse;
+
+    string compile_info_string = R"({
+                                        "hardware_info": {
+                                            "BT_SIZE": 1024,
+                                            "load3d_constraints": "0",
+                                            "Intrinsic_fix_pipe_l0c2out": true,
+                                            "Intrinsic_data_move_l12ub": false,
+                                            "Intrinsic_data_move_l0c2ub": false,
+                                            "Intrinsic_data_move_out2l1_nd2nz": true,
+                                            "UB_SIZE": 196608,
+                                            "L2_SIZE": 33554432,
+                                            "L1_SIZE": 524288,
+                                            "L0A_SIZE": 65536,
+                                            "L0B_SIZE": 65536,
+                                            "L0C_SIZE": 131072,
+                                            "CORE_NUM": 20
+                                        }
+                                    })";
+    map<string, string> soc_infos;
+    map<string, string> aicore_spec;
+    map<string, string> intrinsics;
+    GetPlatFormInfos(compile_info_string.c_str(), soc_infos, aicore_spec, intrinsics);
+
+    // platform info
+    fe::PlatFormInfos platform_info;
+    platform_info.Init();
+    // compile info
+    struct GroupedMatMulAllReduceCompileInfo {
+    } compile_info;
+
+    // tilingParseFunc simulate
+    auto kernel_holder =
+        gert::KernelRunContextFaker()
+            .KernelIONum(4, 1)
+            .Inputs({const_cast<char*>(compile_info_string.c_str()), reinterpret_cast<void*>(&platform_info)})
+            .Outputs({&compile_info})
+            .Build();
+
+    // tilingFunc simulate
+    auto param = gert::TilingData::CreateCap(12016);
+    ASSERT_NE(param, nullptr);
+    auto workspace_size_holer = gert::ContinuousVector::Create<size_t>(4096);
+    auto ws_size = reinterpret_cast<gert::ContinuousVector*>(workspace_size_holer.get());
+    gert::StorageShape xShape = {{1280, 345}, {1280, 345}};
+    gert::StorageShape wShape = {{1280, 567}, {1280, 567}};
+    gert::StorageShape groupListShape = {{2}, {2}};
+    gert::StorageShape yShape = {{690, 567}, {690, 567}};
+
+    auto holder = gert::TilingContextFaker()
+                      .NodeIoNum(4, 1)
+                      .IrInstanceNum({1, 1, 1, 1})
+                      .InputShapes({&xShape, &wShape, &groupListShape, &yShape})
+                      .OutputShapes({&yShape})
+                      .NodeAttrs(
+                          {{"transpose_weight", ge::AnyValue::CreateFrom<bool>(false)},
+                           {"transpose_x", ge::AnyValue::CreateFrom<bool>(true)}})
+                      .CompileInfo(&compile_info)
+                      .PlatformInfo(reinterpret_cast<char*>(&platform_info))
+                      .NodeInputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(1, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(2, ge::DT_INT64, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeInputTd(3, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .NodeOutputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                      .TilingData(param.get())
+                      .Workspace(ws_size)
+                      .SetOpType(op_type)
+                      .Build();
+
+    gert::TilingContext* tiling_context = holder.GetContext<gert::TilingContext>();
+    ASSERT_NE(tiling_context->GetPlatformInfo(), nullptr);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
+    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap", intrinsics);
+
+    EXPECT_EQ(tiling_func(tiling_context), ge::GRAPH_SUCCESS);
+
+    // to check tiling result
+    auto tiling_key = tiling_context->GetTilingKey();
+    ASSERT_EQ(tiling_key, 0);
+}
