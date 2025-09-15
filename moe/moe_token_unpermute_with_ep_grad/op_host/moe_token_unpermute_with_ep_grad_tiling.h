bb90fa511aebe7688d6dc5b7edaa9d863ee6407a
chen-linxin4 <chenlinxin4@huawei.com>
2025-09-15 19:16:25 +0800

    fix include
    Co-authored-by: 陈琳鑫<chenlinxin4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge fixinclude into master
    
    fix include
    
    Created-by: chen-linxin4
    Commit-by: 陈琳鑫
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!220

--- moe/moe_token_unpermute_with_ep_grad/op_host/moe_token_unpermute_with_ep_grad_tiling.h
+++ moe/moe_token_unpermute_with_ep_grad/op_host/moe_token_unpermute_with_ep_grad_tiling.h
@@ -25,7 +25,7 @@
 #include "log/log.h"
 #include "register/op_impl_registry.h"
 #include "register/tilingdata_base.h"
-#include "tiling/tiling_base.h"
+#include "tiling_base/tiling_base.h"
 #include "tiling/tiling_templates_registry.h"
 #include "util/math_util.h"
 
