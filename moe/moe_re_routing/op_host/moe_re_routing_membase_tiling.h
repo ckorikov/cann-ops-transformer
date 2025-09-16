4f95aa6657d9aee6b6c0dccbfe937da162bf4010
liu-zihao35 <liuzihao35@h-partners.com>
2025-09-16 21:51:34 +0800

    开源整改 moe_re_routing
    Co-authored-by: liu-zihao35<liuzihao35@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge MoeReRouting into master
    
    开源整改 moe_re_routing
    
    Created-by: Alen2434
    Commit-by: liu-zihao35
    Merged-by: ascend-robot
    Description: moe_re_routing
    
    See merge request: cann/ops-transformer-dev!165

---
+++ moe/moe_re_routing/op_host/moe_re_routing_membase_tiling.h
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file moe_re_routing_membase_tiling.h
+ * \brief
+ */
+#ifndef OPS_BUILT_IN_OP_TILING_RUNTIME_MOE_RE_ROUTING_MEMBASE_H_
+#define OPS_BUILT_IN_OP_TILING_RUNTIME_MOE_RE_ROUTING_MEMBASE_H_
+
+#include "moe_re_routing_tiling.h"
+
+namespace optiling {
+
+class MoeReRoutingTiling : public TilingBaseClass {
+public:
+    explicit MoeReRoutingTiling(gert::TilingContext *context_) : TilingBaseClass(context_)
+    {}
+    ~MoeReRoutingTiling() override = default;
+
+    int64_t coreNum_ = 0;
+    int64_t ubSize_ = 0;
+
+protected:
+    bool IsCapable() override;
+    ge::graphStatus DoOpTiling() override;
+    ge::graphStatus DoLibApiTiling() override;
+    uint64_t GetTilingKey() const override;
+    ge::graphStatus GetPlatformInfo() override;
+    ge::graphStatus GetShapeAttrsInfo() override;
+    ge::graphStatus GetWorkspaceSize() override;
+    ge::graphStatus PostTiling() override;
+
+private:
+    uint64_t tilingKey_ = 0;
+    platform_ascendc::SocVersion socVersion_ = platform_ascendc::SocVersion::ASCEND910B;
+    MoeReRoutingTilingData tilingData_;
+    ParamsMoeReRouting commonParams_;
+};
+
+}  // namespace optiling
+
+#endif  // OPS_BUILT_IN_OP_TILING_RUNTIME_MOE_RE_ROUTING_MEMBASE_H_
