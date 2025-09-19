ff02f3382493ef387ff85fba87a7a91d0471fa0a
yangbinhua <yangbinhua1@huawei.com>
2025-09-19 22:21:53 +0800

    modify form
    Co-authored-by: yangbinhua<yangbinhua1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    modify form
    
    Created-by: yangbinhua
    Commit-by: yangbinhua
    Merged-by: ascend-robot
    Description: ## 描述
    修改文件开头不符合规范的license格式
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!382

--- cmake/makeself_built_in.cmake
+++ cmake/makeself_built_in.cmake
@@ -1,4 +1,4 @@
-
+# ----------------------------------------------------------------------------
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
 # This file is a part of the CANN Open Software.
 # Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
@@ -6,7 +6,7 @@
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
-# ============================================================================
+# ----------------------------------------------------------------------------
 # makeself.cmake - 自定义 makeself 打包脚本
 
 # 设置 makeself 路径
