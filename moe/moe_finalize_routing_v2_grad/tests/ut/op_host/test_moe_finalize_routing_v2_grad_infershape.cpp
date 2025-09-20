218ab09d25be75808b9b7362fc552918be139cf6
ascend-robot <zhongyuanke@huawei.com>
2025-09-20 19:28:32 +0800

    开源迁仓 MoeFinalizeRoutingV2Grad
    Co-authored-by: Qianwen Ye<yeqianwen2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge MoeFinalizeRoutingV2Grad into master
    
    开源迁仓 MoeFinalizeRoutingV2Grad
    
    Created-by: Wei_NaChuan
    Commit-by: Qianwen Ye
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!374

---
+++ moe/moe_finalize_routing_v2_grad/tests/ut/op_host/test_moe_finalize_routing_v2_grad_infershape.cpp
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "op_proto_test_util.h"
+#include "fusion_ops.h"
+#include "graph/utils/op_desc_utils.h"
+#include "common/utils/ut_op_common.h"
+
+class MoeFinalizeRoutingV2GradProto : public testing::Test
+{
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "MoeFinalizeRoutingV2GradProto SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "MoeFinalizeRoutingV2GradProto TearDown" << std::endl;
+    }
+};
+
+TEST_F(MoeFinalizeRoutingV2GradProto, shape_infer)
+{
+    ge::op::MoeFinalizeRoutingV2Grad op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
+
+    auto grad_y_tensor_desc =
+        create_desc_shape_range({5, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("grad_y", grad_y_tensor_desc);
+
+    auto expanded_row_idx_tensor_desc =
+        create_desc_shape_range({15}, ge::DT_INT32, ge::FORMAT_ND, {15}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
+
+    auto expanded_x_tensor_desc =
+        create_desc_shape_range({15, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {15, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_x", expanded_x_tensor_desc);
+
+    auto scales_tensor_desc =
+        create_desc_shape_range({5, 3}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 3}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("scales", scales_tensor_desc);
+
+    auto expert_idx_tensor_desc =
+        create_desc_shape_range({5, 3}, ge::DT_INT32, ge::FORMAT_ND, {5, 3}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expert_idx", expert_idx_tensor_desc);
+
+    auto bias_tensor_desc =
+        create_desc_shape_range({8, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {8, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+
+    auto ret = InferShapeTest(op);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto grad_expanded_x_tensor_desc = op.GetOutputDescByName("grad_expanded_x");
+    std::vector<int64_t> grad_expanded_x_tensor_expected_shape = {15, 8};
+    EXPECT_EQ(grad_expanded_x_tensor_desc.GetShape().GetDims(), grad_expanded_x_tensor_expected_shape);
+
+    auto grad_scales_tensor_desc = op.GetOutputDescByName("grad_scales");
+    std::vector<int64_t> grad_scales_tensor_expected_shape = {5, 3};
+    EXPECT_EQ(grad_scales_tensor_desc.GetShape().GetDims(), grad_scales_tensor_expected_shape);
+}
+
+TEST_F(MoeFinalizeRoutingV2GradProto, dtype_infer)
+{
+    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl("MoeFinalizeRoutingV2Grad"), nullptr);
+    auto data_type_func = gert::OpImplRegistry::GetInstance().GetOpImpl("MoeFinalizeRoutingV2Grad")->infer_datatype;
+
+    if (data_type_func != nullptr) {
+        ge::DataType input_ref1 = ge::DT_FLOAT;
+        ge::DataType input_ref2 = ge::DT_INT32;
+        ge::DataType output_ref = ge::DT_FLOAT;
+        auto context_holder =
+            gert::InferDataTypeContextFaker()
+                .NodeIoNum(6, 2)
+                .IrInstanceNum({1, 1, 1, 1, 1, 1})
+                .NodeInputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeInputTd(1, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeInputTd(2, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeInputTd(3, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeInputTd(4, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeInputTd(5, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeOutputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .NodeOutputTd(1, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                .InputDataTypes({&input_ref1, &input_ref2, &input_ref1, &input_ref1, &input_ref2, &input_ref1})
+                .OutputDataTypes({&output_ref, &output_ref})
+                .Build();
+        auto context = context_holder.GetContext<gert::InferDataTypeContext>();
+        EXPECT_EQ(data_type_func(context), ge::GRAPH_SUCCESS);
+        ASSERT_NE(context, nullptr);
+
+        EXPECT_EQ(context->GetOutputDataType(0), output_ref);
+        EXPECT_EQ(context->GetOutputDataType(1), output_ref);
+    }
+}
+
+TEST_F(MoeFinalizeRoutingV2GradProto, invalid_shape_infer_0)
+{
+    ge::op::MoeFinalizeRoutingV2Grad op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
+
+    auto grad_y_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("grad_y", grad_y_tensor_desc);
+
+    auto expanded_row_idx_tensor_desc =
+        create_desc_shape_range({15}, ge::DT_INT32, ge::FORMAT_ND, {15}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
+
+    auto ret = InferShapeTest(op);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto grad_expanded_x_tensor_desc = op.GetOutputDescByName("grad_expanded_x");
+    std::vector<int64_t> grad_expanded_x_tensor_expected_shape = {-2};
+    EXPECT_EQ(grad_expanded_x_tensor_desc.GetShape().GetDims(), grad_expanded_x_tensor_expected_shape);
+
+    auto grad_scales_tensor_desc = op.GetOutputDescByName("grad_scales");
+    std::vector<int64_t> grad_scales_tensor_expected_shape = {-2};
+    EXPECT_EQ(grad_scales_tensor_desc.GetShape().GetDims(), grad_scales_tensor_expected_shape);
+}
+
+TEST_F(MoeFinalizeRoutingV2GradProto, invalid_shape_infer_1)
+{
+    ge::op::MoeFinalizeRoutingV2Grad op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
+
+    auto grad_y_tensor_desc =
+        create_desc_shape_range({15, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("grad_y", grad_y_tensor_desc);
+
+    auto expanded_row_idx_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_INT32, ge::FORMAT_ND, {15}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
+
+    auto ret = InferShapeTest(op);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto grad_expanded_x_tensor_desc = op.GetOutputDescByName("grad_expanded_x");
+    std::vector<int64_t> grad_expanded_x_tensor_expected_shape = {-2};
+    EXPECT_EQ(grad_expanded_x_tensor_desc.GetShape().GetDims(), grad_expanded_x_tensor_expected_shape);
+
+    auto grad_scales_tensor_desc = op.GetOutputDescByName("grad_scales");
+    std::vector<int64_t> grad_scales_tensor_expected_shape = {-2};
+    EXPECT_EQ(grad_scales_tensor_desc.GetShape().GetDims(), grad_scales_tensor_expected_shape);
+}
+
+TEST_F(MoeFinalizeRoutingV2GradProto, shape_infer_option_param_shape_invalid)
+{
+    ge::op::MoeFinalizeRoutingV2Grad op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
+
+    auto grad_y_tensor_desc =
+        create_desc_shape_range({5, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("grad_y", grad_y_tensor_desc);
+
+    auto expanded_row_idx_tensor_desc =
+        create_desc_shape_range({15}, ge::DT_INT32, ge::FORMAT_ND, {15}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
+
+    auto expanded_x_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_FLOAT, ge::FORMAT_ND, {15, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_x", expanded_x_tensor_desc);
+
+    auto scales_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 3}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("scales", scales_tensor_desc);
+
+    auto ret = InferShapeTest(op);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+}
+
+TEST_F(MoeFinalizeRoutingV2GradProto, shape_infer_set_drop_mode_on)
+{
+    ge::op::MoeFinalizeRoutingV2Grad op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{1, 1024}};
+
+    auto grad_y_tensor_desc =
+        create_desc_shape_range({5, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("grad_y", grad_y_tensor_desc);
+
+    auto expanded_row_idx_tensor_desc =
+        create_desc_shape_range({15}, ge::DT_INT32, ge::FORMAT_ND, {15}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
+
+    auto expanded_x_tensor_desc =
+        create_desc_shape_range({15, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {15, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expanded_x", expanded_x_tensor_desc);
+
+    auto scales_tensor_desc =
+        create_desc_shape_range({5, 3}, ge::DT_FLOAT, ge::FORMAT_ND, {5, 3}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("scales", scales_tensor_desc);
+
+    auto expert_idx_tensor_desc =
+        create_desc_shape_range({5, 3}, ge::DT_INT32, ge::FORMAT_ND, {5, 3}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("expert_idx", expert_idx_tensor_desc);
+
+    auto bias_tensor_desc =
+        create_desc_shape_range({8, 8}, ge::DT_FLOAT, ge::FORMAT_ND, {8, 8}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+    op.SetAttr("drop_pad_mode", 1);
+
+    auto ret = InferShapeTest(op);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+
+    auto grad_expanded_x_tensor_desc = op.GetOutputDescByName("grad_expanded_x");
+    std::vector<int64_t> grad_expanded_x_tensor_expected_shape = {15, 8};
+    EXPECT_EQ(grad_expanded_x_tensor_desc.GetShape().GetDims(), grad_expanded_x_tensor_expected_shape);
+
+    auto grad_scales_tensor_desc = op.GetOutputDescByName("grad_scales");
+    std::vector<int64_t> grad_scales_tensor_expected_shape = {5, 3};
+    EXPECT_EQ(grad_scales_tensor_desc.GetShape().GetDims(), grad_scales_tensor_expected_shape);
+}
