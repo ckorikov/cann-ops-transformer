d57e97299549f0baf9cfa8609e2d2ee801caf9a2
liu-zihao35 <liuzihao35@h-partners.com>
2025-09-20 19:56:51 +0800

    add moe_gating_top_k adapt readme && aclnn.md
    Co-authored-by: liu-zihao35<liuzihao35@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge GatingTopK_ws_docs into master
    
    add moe_gating_top_k adapt readme && aclnn.md
    
    Created-by: liu-zihao35
    Commit-by: liu-zihao35
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    开源整改 MoeGatingTopK，补充interleave、moe_re_routing、moe_gating_top_k文档
    /kind task
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!409

--- docs/README.md
+++ docs/README.md
@@ -8,7 +8,42 @@
 
 以`AddExample`算子为例，介绍开发AI Core算子所需的开发交付件和实现过程，请根据实际情况实现对应算子。
 
-- [AI Core算子开发指南](../docs/context/aicore_develop_guide.md)
+- 算子IR（Intermediate Representation）：表示算子原型，描述了算子输入、输出、属性等信息，包括数据类型、shape、数据格式等。算子清单中有部分算子定义了IR，表明可通过IR构图方式调用算子。
+
+| 算子分类 |  算子目录  |    算子IR    |   说明 |
+|---------|---------|------------|---------|
+|moe|[moe_gating_top_k](../moe/moe_gating_top_k/README.md)|[MoeGatingTopK](../moe/moe_gating_top_k/op_graph/moe_gating_top_k_proto.h)|MoE计算中，对输入x做Sigmoid计算，对计算结果分组进行排序，最后根据分组排序的结果选取前k个专家。|
+|moe|[moe_re_routing](../moe/moe_re_routing/README.md)|[MoeReRouting](../moe/moe_re_routing/op_graph/moe_re_routing_proto.h)|MoE网络中，进行AlltoAll操作从其他卡上拿到需要算的token后，将token按照专家顺序重新排列。|
+|posembedding|[interleave_rope](../posembedding/interleave_rope/README.md)|[InterleaveRope](../posembedding/interleave_rope/op_graph/interleave_rope_proto.h)|针对单输入 x 进行旋转位置编码。|
+|xxx|[xxxx](../xx/xx/graph_plugin/xxx.h)|xxx|xxx|
+
+## 算子接口（aclnn）
+
+为方便调用算子，提供一套基于C的API（以aclnn为前缀API），无需提供IR（Intermediate Representation）定义，方便高效构建模型与应用开发，该方式也被称为“单算子API调用”，其详细介绍请参见[《AOL算子加速库接口》](https://hiascend.com/document/redirect/CannCommunityOplist)。
+
+本项目提供的所有算子接口清单如下：
+
+|    接口名   |      说明     |
+|-----------|------------|
+|[aclnnGroupedMatmulSwigluQuant](../gmm/grouped_matmul_swiglu_quant/docs/aclnnGroupedMatmulSwigluQuant.md)|实现融合GroupedMatmul 、dquant、swiglu和quant运算。|
+|[aclnnGroupedMatmulSwigluQuantWeightNZ](../gmm/grouped_matmul_swiglu_quant/docs/aclnnGroupedMatmulSwigluQuantWeightNZ.md)|实现融合GroupedMatmul 、dquant、swiglu和quant运，是aclnnGroupedMatmulSwigluQuant接口的weightNZ特化版本。|
+|[aclnnMoeGatingTopK](../moe/moe_gating_top_k/docs/aclnnMoeGatingTopK.md)|MoE计算中，对输入x做Sigmoid计算，对计算结果分组进行排序，最后根据分组排序的结果选取前k个专家。|
+
+## 图融合规则
+
+使用图方式描述网络时，可采用图融合提升算子性能。图融合是指[GE（Graph Engine）](https://www.hiascend.com/cann/graph-engine)按融合规则进行改图的过程，使用融合后的算子替换融合前的算子。图融合详细介绍请参见[《图融合和UB融合规则参考》](https://hiascend.com/document/redirect/CannCommunitygraphubfusionref)。
+
+本项目提供的所有融合规则清单如下：
+
+|  规则名  |    说明    |
+|---------|------------|
+|[MatmulxxxPass](../math/xxx/xx.md)|待补充。|
+
+
+## 算子开发指南
+
+本章以`AddExample`算子为例，介绍算子具体的开发过程和交付件，开发者请根据实际算子功能自行修改代码实现。
+- [AI Core算子开发指南](../docs/context/AI%20Core算子开发指南.md)
 
 ## 算子调用
 
---
+++ moe/moe_gating_top_k/tests/ut/op_host/test_moe_gating_top_k_infershape.cpp
@@ -0,0 +1,174 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/* !
+ * \file test_moe_gating_top_k_infershape.cpp
+ * \brief
+ */
+#include <gtest/gtest.h> // NOLINT
+#include <iostream>
+#include "op_proto_test_util.h" // NOLINT
+#include "experiment_ops.h"     // NOLINT
+#include "graph/utils/op_desc_utils.h"
+#include "common/utils/ut_op_common.h"
+
+class MoeGatingTopK : public testing::Test
+{
+protected:
+    static void SetUpTestCase()
+    {
+        std::cout << "MoeGatingTopK SetUp" << std::endl;
+    }
+
+    static void TearDownTestCase()
+    {
+        std::cout << "MoeGatingTopK TearDown" << std::endl;
+    }
+};
+
+TEST_F(MoeGatingTopK, moe_gating_top_k_infer_shape_01)
+{
+    ge::op::MoeGatingTopK op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
+    auto x_tensor_desc =
+        create_desc_shape_range({16, 256}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    auto bias_tensor_desc = create_desc_shape_range(
+        {
+            256,
+        },
+        ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("x", x_tensor_desc);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+    op.SetAttr("k", 8);
+    op.SetAttr("k_group", 4);
+    op.SetAttr("group_count", 8);
+    Runtime2TestParam param{{"k", "k_group", "group_count"}};
+    auto ret = InferShapeTest(op, param);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+    auto y_desc = op.GetOutputDesc("y");
+    auto expert_idx_desc = op.GetOutputDesc("expert_idx");
+    auto out_desc = op.GetOutputDesc("out");
+    std::vector<int64_t> y_shape = {16, 8};
+    std::vector<int64_t> expert_idx_shape = {16, 8};
+    std::vector<int64_t> out_shape = {16, 256};
+    EXPECT_EQ(y_desc.GetShape().GetDims(), y_shape);
+    EXPECT_EQ(expert_idx_desc.GetShape().GetDims(), expert_idx_shape);
+    EXPECT_EQ(out_desc.GetShape().GetDims(), out_shape);
+}
+
+TEST_F(MoeGatingTopK, moe_gating_top_k_infer_shape_02)
+{
+    ge::op::MoeGatingTopK op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
+    auto x_tensor_desc = create_desc_shape_range({-2}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    auto bias_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("x", x_tensor_desc);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+    op.SetAttr("k", 8);
+    op.SetAttr("k_group", 4);
+    op.SetAttr("group_count", 8);
+    Runtime2TestParam param{{"k", "k_group", "group_count"}};
+    auto ret = InferShapeTest(op, param);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+    auto y_desc = op.GetOutputDesc("y");
+    auto expert_idx_desc = op.GetOutputDesc("expert_idx");
+    auto out_desc = op.GetOutputDesc("out");
+    std::vector<int64_t> y_shape = {-1, 8};
+    std::vector<int64_t> expert_idx_shape = {-1, 8};
+    std::vector<int64_t> out_shape = {-1, -1};
+    EXPECT_EQ(y_desc.GetShape().GetDims(), y_shape);
+    EXPECT_EQ(expert_idx_desc.GetShape().GetDims(), expert_idx_shape);
+    EXPECT_EQ(out_desc.GetShape().GetDims(), out_shape);
+}
+
+TEST_F(MoeGatingTopK, moe_gating_top_k_infer_shape_03)
+{
+    ge::op::MoeGatingTopK op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
+    auto x_tensor_desc =
+        create_desc_shape_range({-1, -1}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    auto bias_tensor_desc =
+        create_desc_shape_range({-2}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("x", x_tensor_desc);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+    op.SetAttr("k", 8);
+    op.SetAttr("k_group", 4);
+    op.SetAttr("group_count", 8);
+    Runtime2TestParam param{{"k", "k_group", "group_count"}};
+    auto ret = InferShapeTest(op, param);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+    auto y_desc = op.GetOutputDesc("y");
+    auto expert_idx_desc = op.GetOutputDesc("expert_idx");
+    auto out_desc = op.GetOutputDesc("out");
+    std::vector<int64_t> y_shape = {-1, 8};
+    std::vector<int64_t> expert_idx_shape = {-1, 8};
+    std::vector<int64_t> out_shape = {-1, -1};
+    EXPECT_EQ(y_desc.GetShape().GetDims(), y_shape);
+    EXPECT_EQ(expert_idx_desc.GetShape().GetDims(), expert_idx_shape);
+    EXPECT_EQ(out_desc.GetShape().GetDims(), out_shape);
+}
+
+TEST_F(MoeGatingTopK, moe_gating_top_k_infer_shape_04)
+{
+    ge::op::MoeGatingTopK op;
+    std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
+    auto x_tensor_desc =
+        create_desc_shape_range({-1, -1}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    auto bias_tensor_desc =
+        create_desc_shape_range({-1}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
+    op.UpdateInputDesc("x", x_tensor_desc);
+    op.UpdateInputDesc("bias", bias_tensor_desc);
+    op.SetAttr("k", 8);
+    op.SetAttr("k_group", 4);
+    op.SetAttr("group_count", 8);
+    Runtime2TestParam param{{"k", "k_group", "group_count"}};
+    auto ret = InferShapeTest(op, param);
+    EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+    auto y_desc = op.GetOutputDesc("y");
+    auto expert_idx_desc = op.GetOutputDesc("expert_idx");
+    auto out_desc = op.GetOutputDesc("out");
+    std::vector<int64_t> y_shape = {-1, 8};
+    std::vector<int64_t> expert_idx_shape = {-1, 8};
+    std::vector<int64_t> out_shape = {-1, -1};
+    EXPECT_EQ(y_desc.GetShape().GetDims(), y_shape);
+    EXPECT_EQ(expert_idx_desc.GetShape().GetDims(), expert_idx_shape);
+    EXPECT_EQ(out_desc.GetShape().GetDims(), out_shape);
+}
+
+TEST_F(MoeGatingTopK, moe_gating_top_k_infer_datatype_01)
+{
+    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl("MoeGatingTopK"), nullptr);
+    auto data_type_func = gert::OpImplRegistry::GetInstance().GetOpImpl("MoeGatingTopK")->infer_datatype;
+
+    if (data_type_func != nullptr) {
+        ge::DataType input_ref = ge::DT_FLOAT;
+        ge::DataType output_ref = ge::DT_FLOAT;
+        ge::DataType rstd_ref = ge::DT_INT32;
+        auto context_holder = gert::InferDataTypeContextFaker()
+                                  .IrInputNum(3)
+                                  .NodeIoNum(3, 3)
+                                  .NodeInputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeInputTd(1, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(0, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(1, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .NodeOutputTd(2, ge::DT_FLOAT, ge::FORMAT_ND, ge::FORMAT_ND)
+                                  .InputDataTypes({&input_ref, &input_ref})
+                                  .OutputDataTypes({&output_ref, &rstd_ref, &output_ref})
+                                  .Build();
+        auto context = context_holder.GetContext<gert::InferDataTypeContext>();
+        EXPECT_EQ(data_type_func(context), ge::GRAPH_SUCCESS);
+        ASSERT_NE(context, nullptr);
+        EXPECT_EQ(context->GetInputDataType(0), input_ref);
+        EXPECT_EQ(context->GetOutputDataType(0), output_ref);
+        EXPECT_EQ(context->GetOutputDataType(1), rstd_ref);
+        EXPECT_EQ(context->GetOutputDataType(2), output_ref);
+    }
+}
