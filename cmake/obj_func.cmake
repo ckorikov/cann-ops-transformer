400d65c25e81ab5a50e4080e4a611cbc264b6fec
wangsong96 <wangsong109@huawei.com>
2025-09-24 11:46:07 +0800

    修复推理FA的build_in和custom
    Co-authored-by: wangsong96<wangsong109@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    修复推理FA的build_in和custom
    
    Created-by: wangsong96
    Commit-by: wangsong96
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!562

--- cmake/obj_func.cmake
+++ cmake/obj_func.cmake
@@ -34,11 +34,6 @@ macro(add_modules_sources)
   get_filename_component(PARENT_DIR ${SOURCE_DIR} DIRECTORY)
   get_filename_component(OP_NAME ${PARENT_DIR} NAME)
   list(FIND ASCEND_OP_NAME ${OP_NAME} INDEX)
-  if((NOT "${ASCEND_OP_NAME}" STREQUAL "ALL") AND
-      (INDEX EQUAL -1))
-    #ASCEND_OP_NAME 为"ALL"表示全部编译
-    return()
-  endif()
   # 记录全局的COMPILED_OPS和COMPILED_OP_DIRS，其中COMPILED_OP_DIRS只记录到算子名，例如moe/moe_token_permute_with_routing_map_grad
   set(COMPILED_OPS ${COMPILED_OPS} ${OP_NAME} CACHE STRING "Compiled Ops" FORCE)
   set(COMPILED_OP_DIRS ${COMPILED_OP_DIRS} ${PARENT_DIR} CACHE STRING "Compiled Ops Dirs" FORCE)
