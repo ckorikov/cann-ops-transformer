15f68baa34484214555ad52cafb339d5049e3028
Wei_NaChuan <weinachuan1@huawei.com>
2025-09-28 17:33:46 +0800

    add moe_init_routing_v2_grad UT
    Co-authored-by: Wei_NaChuan<weinachuan1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge UT-MoeInitRoutingV2Grad into master
    
    add moe_init_routing_v2_grad UT
    
    Created-by: Wei_NaChuan
    Commit-by: Wei_NaChuan
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!895

--- moe/moe_init_routing_v2_grad/tests/ut/op_host/test_moe_init_routing_v2_grad_infershape.cpp
+++ moe/moe_init_routing_v2_grad/tests/ut/op_host/test_moe_init_routing_v2_grad_infershape.cpp
@@ -8,14 +8,12 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include <gtest/gtest.h>  // NOLINT
+#include <gtest/gtest.h>
 #include <iostream>
-#include "op_proto_test_util.h"  // NOLINT
-#include "fusion_ops.h"      // NOLINT
-#include "graph/utils/op_desc_utils.h"
-#include "common/utils/ut_op_common.h"
+#include "infershape_context_faker.h"
+#include "base/registry/op_impl_space_registry_v2.h"
 
-class MoeInitRoutingV2Grad : public testing::Test {
+class MoeInitRoutingV2GradInferShape : public testing::Test {
  protected:
   static void SetUpTestCase() {
     std::cout << "MoeInitRoutingV2Grad SetUp" << std::endl;
@@ -26,201 +24,71 @@ class MoeInitRoutingV2Grad : public testing::Test {
   }
 };
 
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_01) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({-2}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({-2}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 6);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
-  auto grad_x_desc = op.GetOutputDesc("grad_x");
-  std::vector<int64_t> grad_x_shape = {-1, -1};
-  EXPECT_EQ(grad_x_desc.GetShape().GetDims(), grad_x_shape);
+struct MoeInitRoutingV2GradInfo {
+    gert::StorageShape &gradExpandedXShape;
+    gert::StorageShape &expandedRowIdxShape;
+    std::vector<int64_t> expectOutShape;
+
+    ge::DataType gradExpandedXDtype;
+    ge::DataType expandedRowIdxDtype;
+    ge::DataType yDtype;
+
+    int64_t topK = 0;
+    int64_t dropPadMode = 0;
+    int64_t active_num = 0;
+};
+
+static std::vector<int64_t> ToVector(const gert::Shape &shape)
+{
+    size_t shapeSize = shape.GetDimNum();
+    std::vector<int64_t> shapeVec(shapeSize, 0);
+    for (size_t i = 0; i < shapeSize; i++) {
+      shapeVec[i] = shape.GetDim(i);
+    }
+    return shapeVec;
 }
 
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_02) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({-1, -1}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({-1}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 6);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
-  auto grad_x_desc = op.GetOutputDesc("grad_x");
-  std::vector<int64_t> grad_x_shape = {-1, -1};
-  EXPECT_EQ(grad_x_desc.GetShape().GetDims(), grad_x_shape);
+static void ExeTestCase(const MoeInitRoutingV2GradInfo ioInfo, ge::graphStatus testCaseResult = ge::GRAPH_SUCCESS)
+{
+    /* make infershape context */
+    gert::StorageShape yStorageShape = {};
+    std::vector<gert::StorageShape *> ouputShapes = {&yStorageShape};
+    auto contextHolder = gert::InferShapeContextFaker()
+                             .SetOpType("MoeInitRoutingV2Grad")
+                             .NodeIoNum(2, 1)
+                             .NodeInputTd(0, ioInfo.gradExpandedXDtype, ge::FORMAT_ND, ge::FORMAT_ND)
+                             .NodeInputTd(1, ioInfo.expandedRowIdxDtype, ge::FORMAT_ND, ge::FORMAT_ND)
+                             .NodeOutputTd(0, ioInfo.yDtype, ge::FORMAT_ND, ge::FORMAT_ND)
+
+                             .InputTensors({(gert::Tensor *)&ioInfo.gradExpandedXShape})
+                             .InputTensors({(gert::Tensor *)&ioInfo.expandedRowIdxShape})
+                             .Attr("top_k", int64_t(ioInfo.topK))
+                             .Attr("drop_pad_mode", int64_t(ioInfo.dropPadMode))
+                             .Attr("active_num", int64_t(ioInfo.active_num))
+                             .OutputShapes(ouputShapes)
+                             .Build();
+
+    /* get infershape func */
+    auto spaceRegistry = gert::DefaultOpImplSpaceRegistryV2::GetInstance().GetSpaceRegistry();
+    auto inferShapeFunc = spaceRegistry->GetOpImpl("MoeInitRoutingV2Grad")->infer_shape;
+    ASSERT_NE(inferShapeFunc, nullptr);
+
+    /* do infershape */
+    EXPECT_EQ(inferShapeFunc(contextHolder.GetContext()), testCaseResult);
+    EXPECT_EQ(ToVector(yStorageShape.GetOriginShape()), ioInfo.expectOutShape);
 }
 
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_03) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
-  auto grad_x_desc = op.GetOutputDesc("grad_x");
-  std::vector<int64_t> grad_x_shape = {16, 512};
-  EXPECT_EQ(grad_x_desc.GetShape().GetDims(), grad_x_shape);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_04) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 0);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_05) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 2);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_06) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", -1);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_07) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", -1);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_08) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 2);
-  op.SetAttr("active_num", -1);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_09) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({-1, -1, -1}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({-1}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 6);
-  op.SetAttr("drop_pad_mode", 1);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
-  auto grad_x_desc = op.GetOutputDesc("grad_x");
-  std::vector<int64_t> grad_x_shape = {-1, -1};
-  EXPECT_EQ(grad_x_desc.GetShape().GetDims(), grad_x_shape);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_10) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({-1, -1}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({-1}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 6);
-  op.SetAttr("drop_pad_mode", 1);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
-}
-
-TEST_F(MoeInitRoutingV2Grad, moe_init_routing_v2_grad_infer_shape_11) {
-  ge::op::MoeInitRoutingV2Grad op;
-  std::vector<std::pair<int64_t, int64_t>> shape_range = {{2, 64}};
-  auto grad_expanded_x_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_FLOAT16, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  auto expanded_row_idx_tensor_desc =
-      create_desc_shape_range({1024, 512}, ge::DT_INT32, ge::FORMAT_ND, {32}, ge::FORMAT_ND, shape_range);
-  op.UpdateInputDesc("grad_expanded_x", grad_expanded_x_tensor_desc);
-  op.UpdateInputDesc("expanded_row_idx", expanded_row_idx_tensor_desc);
-  op.SetAttr("top_k", 64);
-  op.SetAttr("drop_pad_mode", 0);
-  op.SetAttr("active_num", 0);
-  Runtime2TestParam param{{"top_k", "drop_pad_mode", "active_num"}};
-  auto ret = InferShapeTest(op, param);
-  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+TEST_F(MoeInitRoutingV2GradInferShape, moe_init_routing_v2_grad_infershape_0)
+{
+    gert::StorageShape gradExpandedXShape = {{16, 16}, {16, 16}};
+    gert::StorageShape expandedRowIdxShape = {{16}, {16}};
+    std::vector<int64_t> expectOutShape = {2, 16}; // scale第0维，expandedX第一维
+    MoeInitRoutingV2GradInfo ioInfoT = {gradExpandedXShape,
+                                      expandedRowIdxShape,
+                                      expectOutShape,
+                                      ge::DT_FLOAT,
+                                      ge::DT_INT32,
+                                      ge::DT_FLOAT,
+                                      6, 0, 0};
+    ExeTestCase(ioInfoT, ge::GRAPH_SUCCESS);
 }
