124f8c32976a363e8aeab844dc92dbdbbf31c8cc
yaolu_happy <yaolu15@huawei.com>
2025-09-28 20:33:54 +0800

    删除MIRV3分流V2
    Co-authored-by: yaolu_happy<yaolu15@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge delete_mirv2 into master
    
    删除MIRV3分流V2
    
    Created-by: zhangjinru
    Commit-by: yaolu_happy
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!814

--- moe/moe_init_routing_v3/examples/test_aclnn_moe_init_routing_v3.cpp
+++ moe/moe_init_routing_v3/examples/test_aclnn_moe_init_routing_v3.cpp
@@ -21,7 +21,7 @@
     } while (0)
 #define LOG_PRINT(message, ...)         \
     do {                                \
-        printf(message, ##__VA_ARGS__); \哎
+        printf(message, ##__VA_ARGS__); \
     } while (0)
 int64_t GetShapeSize(const std::vector<int64_t> &shape)
 {
