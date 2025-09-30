182f1ec552ab440184c734f1604965fdc64d6ea1
pencho <wangpengchong1@h-partners.com>
2025-09-30 10:22:44 +0800

    resolve FA warning
    Co-authored-by: pencho<wangpengchong1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge 0929FACleancode into master
    
    resolve FA warning
    
    Created-by: pencho
    Commit-by: pencho
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1033

--- attention/flash_attention_score/op_host/flash_attention_score_tiling.cpp
+++ attention/flash_attention_score/op_host/flash_attention_score_tiling.cpp
@@ -95,7 +95,7 @@ void FlashAttentionScoreEmptyInputTiling::GetTilingKeyAttentionScore4EmptyInput(
 }
 
 void FlashAttentionScoreEmptyInputTiling::FlashAttentionScoreSetEmptyInputTilingData(
-    gert::TilingContext *context, FlashAttentionScoreTilingData &faTilingData)
+    gert::TilingContext *context, [[maybe_unused]] FlashAttentionScoreTilingData &faTilingData)
 {
     OP_CHECK_IF(context->GetRawTilingData() == nullptr,
         OP_LOGE(context, "FlashAttentionScoreSetEmptyInputTilingData occurs nullptr!"),
