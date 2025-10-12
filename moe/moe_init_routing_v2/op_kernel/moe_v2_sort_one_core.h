f04fdf3c7cd1c9b28f8d5245a6e854aa65394957
kdy18482276080 <kongdeyuan3@huawei.com>
2025-10-12 11:44:05 +0800

    fix moeinitroutingv2 310p
    Co-authored-by: 184822760080<kongdeyuan3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1178 merge v2fix into master
    
    fix moeinitroutingv2 310p
    
    Created-by: kdy18482276080
    Commit-by: 184822760080
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    	修复多核排序起始offset
        修复初始化gm非对齐长度
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1178

--- moe/moe_init_routing_v2/op_kernel/moe_v2_sort_one_core.h
+++ moe/moe_init_routing_v2/op_kernel/moe_v2_sort_one_core.h
@@ -170,12 +170,20 @@ __aicore__ inline void MoeV2SortOneCore::Init(GM_ADDR expertIdx, GM_ADDR expertT
         if (this->expertTokensCountOrCumsumFlag > 0) {
             expertTokensCountOrCumsumGm.SetGlobalBuffer((__gm__ int32_t *)expertTokensCountOrCumsum,
                                                         Align(this->expertNum, sizeof(int32_t)));
+#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 200
+            InitGlobalMemory(expertTokensCountOrCumsumGm, Align(this->expertNum, sizeof(int32_t)), 0);
+#else
             InitGlobalMemory(expertTokensCountOrCumsumGm, this->expertNum, 0);
+#endif
         }
         if (this->expertTokensBeforeCapacityFlag == 1) {
             expertTokensBeforeCapacityGm.SetGlobalBuffer((__gm__ int32_t *)expertTokensBeforeCapacity,
                                                          Align(this->expertNum, sizeof(int32_t)));
+#if defined(__CCE_AICORE__) && __CCE_AICORE__ == 200
+            InitGlobalMemory(expertTokensBeforeCapacityGm, Align(this->expertNum, sizeof(int32_t)), 0);
+#else
             InitGlobalMemory(expertTokensBeforeCapacityGm, this->expertNum, 0);
+#endif
         }
     }
     // key and value
