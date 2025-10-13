042d7394e9929b4d6cc05640a99991c219216697
weixin_45523946 <chenjunzhuo@huawei.com>
2025-10-13 20:48:44 +0800

    test_rope_graph
    Co-authored-by: weixin_45523946<chenjunzhuo@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1218 merge test_rope_graph into master
    
    test_rope_graph
    
    Created-by: weixin_45523946
    Commit-by: weixin_45523946
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1218

--- posembedding/rotary_position_embedding/op_host/rotary_position_embedding_tiling.cpp
+++ posembedding/rotary_position_embedding/op_host/rotary_position_embedding_tiling.cpp
@@ -61,7 +61,7 @@ ge::graphStatus Tiling4RotaryPositionEmbedding(gert::TilingContext *context)
 
 ge::graphStatus TilingPrepareForRotaryPositionEmbedding(gert::TilingParseContext *context)
 {
-    OP_LOGE(context, "TilingPrepareForRotaryPositionEmbedding context success");
+    OP_LOGD(context, "TilingPrepareForRotaryPositionEmbedding context success");
     return ge::GRAPH_SUCCESS;
 }
 
