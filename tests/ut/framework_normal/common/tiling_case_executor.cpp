16a61d6364a7180889fff98db4fb2c33e2da48b2
xutianze <xutianze2@huawei.com>
2025-10-13 19:11:04 +0800

    tiling ut 参数为默认值时不校验tilingData和workspace
    Co-authored-by: xutianze<xutianze2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1222 merge mc2_ut_common into master
    
    tiling ut 参数为默认值时不校验tilingData和workspace
    
    Created-by: xutianze
    Commit-by: xutianze
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1222

--- tests/ut/framework_normal/common/tiling_case_executor.cpp
+++ tests/ut/framework_normal/common/tiling_case_executor.cpp
@@ -217,11 +217,13 @@ void ExecuteTestCase(const gert::TilingContextPara& tilingContextPara,
     ASSERT_EQ(tilingKeyResult, expectTilingKey);
 
     // check tiling data
-    auto rawTilingData = tilingContext->GetRawTilingData();
-    auto tilingDataReservedSize = tilingDataReservedLen * sizeof(uint64_t);
-    auto tilingDataResult = to_string<int64_t>(rawTilingData->GetData() + tilingDataReservedSize,
-                                               rawTilingData->GetDataSize() - tilingDataReservedSize);
-    EXPECT_EQ(tilingDataResult, expectTilingData);
+    if (expectTilingData != "") {
+        auto rawTilingData = tilingContext->GetRawTilingData();
+        auto tilingDataReservedSize = tilingDataReservedLen * sizeof(uint64_t);
+        auto tilingDataResult = to_string<int64_t>(rawTilingData->GetData() + tilingDataReservedSize,
+                                                   rawTilingData->GetDataSize() - tilingDataReservedSize);
+        EXPECT_EQ(tilingDataResult, expectTilingData);
+    }
 }
 
 bool ExecuteTiling(const gert::TilingContextPara& tilingContextPara, TilingInfo& tilingInfo)
