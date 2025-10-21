a227b27d0907232edf06862ce394d3a601411690
macech <chenluchun1@huawei.com>
2025-10-21 09:57:16 +0800

    按新格式补充Barrier UT测试用例
    Co-authored-by: macech<chenluchun1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1425 merge cherry-pick-mr-1424-1760968338381-auto into master
    
    按新格式补充Barrier UT测试用例
    
    Created-by: macech
    Commit-by: macech
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1425

--- mc2/distribute_barrier/tests/ut/op_host/op_api/test_aclnn_distribute_barrier.cpp
+++ mc2/distribute_barrier/tests/ut/op_host/op_api/test_aclnn_distribute_barrier.cpp
@@ -14,6 +14,7 @@
 #include "gtest/gtest.h"
 #include <gmock/gmock.h>
 #include "../../../../op_host/op_api/aclnn_distribute_barrier.h"
+#include "../../../../op_host/op_api/aclnn_distribute_barrier_v2.h"
 #include "op_api_ut_common/tensor_desc.h"
 #include "op_api_ut_common/op_api_ut.h"
 #include "opdev/platform.h"
@@ -59,6 +60,68 @@ TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_first_api
 
   auto ut = OP_API_UT(aclnnDistributeBarrier,
                       INPUT(x_ref, "", worldSize),
+                      OUTPUT());
+  uint64_t workspace_size = 0;
+  aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+}
+
+TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_v2_first_api) {
+  TensorDesc x_ref = TensorDesc({3, 4}, ACL_FLOAT16, ACL_FORMAT_ND);
+
+  int64_t worldSize = 16;
+
+  auto ut = OP_API_UT(aclnnDistributeBarrierV2,
+                      INPUT(x_ref, nullptr, nullptr, "test_distribute_barrier", worldSize),
+                      OUTPUT());
+  uint64_t workspace_size = 0;
+  aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+}
+
+TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_v2_first_api_nullptr_group) {
+  TensorDesc x_ref = TensorDesc({3, 4}, ACL_FLOAT16, ACL_FORMAT_ND);
+
+  int64_t worldSize = 16;
+
+  auto ut = OP_API_UT(aclnnDistributeBarrierV2,
+                      INPUT(x_ref, nullptr, nullptr, nullptr, worldSize),
+                      OUTPUT());
+  uint64_t workspace_size = 0;
+  aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+}
+
+TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_v2_first_api_group_min) {
+  TensorDesc x_ref = TensorDesc({3, 4}, ACL_FLOAT16, ACL_FORMAT_ND);
+
+  int64_t worldSize = 16;
+
+  auto ut = OP_API_UT(aclnnDistributeBarrierV2,
+                      INPUT(x_ref, nullptr, nullptr, "", worldSize),
+                      OUTPUT());
+  uint64_t workspace_size = 0;
+  aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+}
+
+TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_v2_first_api_time_out) {
+  TensorDesc x_ref = TensorDesc({3, 4}, ACL_FLOAT16, ACL_FORMAT_ND);
+  TensorDesc time_out = TensorDesc({1}, ACL_INT32, ACL_FORMAT_ND);
+
+  int64_t worldSize = 16;
+
+  auto ut = OP_API_UT(aclnnDistributeBarrierV2,
+                      INPUT(x_ref, time_out, nullptr, "test_distribute_barrier", worldSize),
+                      OUTPUT());
+  uint64_t workspace_size = 0;
+  aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+}
+
+TEST_F(l2_aclnn_distribute_barrier_test, test_aclnn_distribute_barrier_v2_first_api_elastic_info) {
+  TensorDesc x_ref = TensorDesc({3, 4}, ACL_FLOAT16, ACL_FORMAT_ND);
+  TensorDesc elastic_info = TensorDesc({36}, ACL_INT32, ACL_FORMAT_ND);
+
+  int64_t worldSize = 16;
+
+  auto ut = OP_API_UT(aclnnDistributeBarrierV2,
+                      INPUT(x_ref, nullptr, elastic_info, "test_distribute_barrier", worldSize),
                       OUTPUT());
   uint64_t workspace_size = 0;
   aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
--- mc2/distribute_barrier/tests/ut/op_host/test_distribute_barrier_tiling.cpp
+++ mc2/distribute_barrier/tests/ut/op_host/test_distribute_barrier_tiling.cpp
@@ -38,7 +38,7 @@ TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling)
          {"world_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(16)}},
         &compileInfo, "Ascend910_93", coreNum, ubSize);
     uint64_t expectTilingKey = 10000UL;
-    std::string expectTilingData = "8 16 20 196352 0 ";
+    std::string expectTilingData = "8 16 20 196352 0 0 ";
     std::vector<size_t> expectWorkspaces = {16777216};
     uint64_t mc2TilingDataReservedLen = 42;
     ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces,
@@ -73,166 +73,72 @@ TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_world_size_385)
     ExecuteTestCase(tilingContextPara);
 }
 
-TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_elastic_info) {
-    std::string op_type("DistributeBarrier");
-    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str()), nullptr);
-    auto tiling_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling;
-    auto tiling_parse_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling_parse;
-
-    string compile_info_string = R"({
-                                        "hardware_info": {
-                                            "BT_SIZE": 1024,
-                                            "load3d_constraints": "0",
-                                            "Intrinsic_fix_pipe_l0c2out": true,
-                                            "Intrinsic_data_move_l12ub": false,
-                                            "Intrinsic_data_move_l0c2ub": false,
-                                            "Intrinsic_data_move_out2l1_nd2nz": true,
-                                            "UB_SIZE": 196608,
-                                            "L2_SIZE": 33554432,
-                                            "L1_SIZE": 524288,
-                                            "L0A_SIZE": 65536,
-                                            "L0B_SIZE": 65536,
-                                            "L0C_SIZE": 131072,
-                                            "CORE_NUM": 20
-                                        }
-                                    })";
-    map<string, string> soc_infos;
-    map<string, string> aicore_spec;
-    map<string, string> intrinsics;
-    GetPlatFormInfos(compile_info_string.c_str(), soc_infos, aicore_spec, intrinsics);
-
-    // platform info
-    fe::PlatFormInfos platform_info;
-    platform_info.Init();
-    // compile info
-    struct DistributeBarrierCompileInfo {
-    } compile_info;
-
-    // tilingParseFunc simulate
-    auto kernel_holder =
-        gert::KernelRunContextFaker()
-            .KernelIONum(3, 1)
-            .Inputs({const_cast<char*>(compile_info_string.c_str()), reinterpret_cast<void*>(&platform_info)})
-            .Outputs({&compile_info})
-            .Build();
-
-    auto param = gert::TilingData::CreateCap(4096);
-    ASSERT_NE(param, nullptr);
-    auto workspace_size_holer = gert::ContinuousVector::Create<size_t>(4096);
-    auto ws_size = reinterpret_cast<gert::ContinuousVector*>(workspace_size_holer.get());
-    gert::StorageShape x_ref_shape = {{3, 4}, {3, 4}};
-    gert::StorageShape elastic_info_shape = {{36}, {36}};
-    gert::StorageShape x_ref_output_shape = {{3, 4}, {3, 4}};
-
-    std::string group("group");
-    int64_t world_size = 16;
-    auto holder = gert::TilingContextFaker()
-                        .NodeIoNum(3, 1)
-                        .IrInstanceNum({1, 1, 1})
-                        .InputShapes({&x_ref_shape, nullptr, &elastic_info_shape})
-                        .OutputShapes({&x_ref_output_shape})
-                        .NodeAttrs({{"group", ge::AnyValue::CreateFrom<std::string>(group)},
-                                    {"world_size", ge::AnyValue::CreateFrom<int64_t>(world_size)}})
-                        .CompileInfo(&compile_info)
-                        .PlatformInfo(reinterpret_cast<char*>(&platform_info))
-                        .NodeInputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeInputTd(1, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeInputTd(2, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeOutputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .TilingData(param.get())
-                        .Workspace(ws_size)
-                        .Build();
-
-    gert::TilingContext* tiling_context = holder.GetContext<gert::TilingContext>();
-    ASSERT_NE(tiling_context->GetPlatformInfo(), nullptr);
-    std::map<std::string, std::string> version = {{"Short_SoC_version", "Ascend910_93"}};
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("version", version);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap", intrinsics);
-
-    EXPECT_EQ(tiling_func(tiling_context), ge::GRAPH_SUCCESS);
+TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_time_out) 
+{
+    struct DistributeBarrierCompileInfo {} compileInfo;
+    uint64_t coreNum = 20;
+    uint64_t ubSize = 196608;
+    gert::TilingContextPara tilingContextPara("DistributeBarrier",
+        {
+            {{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{{1}, {1}}, ge::DT_INT32, ge::FORMAT_ND},
+            {{}, ge::DT_INT32, ge::FORMAT_ND},
+        },
+        {{{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},},
+        {{"group", Ops::Transformer::AnyValue::CreateFrom<std::string>("group")},
+         {"world_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(16)}},
+        &compileInfo, "Ascend910_93", coreNum, ubSize);
+        uint64_t expectTilingKey = 10000UL;
+    std::string expectTilingData = "8 16 20 196352 0 1 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    uint64_t mc2TilingDataReservedLen = 42;
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces,
+                    mc2TilingDataReservedLen);
 }
 
-TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_time_out) {
-    std::string op_type("DistributeBarrier");
-    ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str()), nullptr);
-    auto tiling_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling;
-    auto tiling_parse_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling_parse;
-
-    string compile_info_string = R"({
-                                        "hardware_info": {
-                                            "BT_SIZE": 1024,
-                                            "load3d_constraints": "0",
-                                            "Intrinsic_fix_pipe_l0c2out": true,
-                                            "Intrinsic_data_move_l12ub": false,
-                                            "Intrinsic_data_move_l0c2ub": false,
-                                            "Intrinsic_data_move_out2l1_nd2nz": true,
-                                            "UB_SIZE": 196608,
-                                            "L2_SIZE": 33554432,
-                                            "L1_SIZE": 524288,
-                                            "L0A_SIZE": 65536,
-                                            "L0B_SIZE": 65536,
-                                            "L0C_SIZE": 131072,
-                                            "CORE_NUM": 20
-                                        }
-                                    })";
-    map<string, string> soc_infos;
-    map<string, string> aicore_spec;
-    map<string, string> intrinsics;
-    GetPlatFormInfos(compile_info_string.c_str(), soc_infos, aicore_spec, intrinsics);
-
-    // platform info
-    fe::PlatFormInfos platform_info;
-    platform_info.Init();
-    // compile info
-    struct DistributeBarrierCompileInfo {
-    } compile_info;
-
-    // tilingParseFunc simulate
-    auto kernel_holder =
-        gert::KernelRunContextFaker()
-            .KernelIONum(3, 1)
-            .Inputs({const_cast<char*>(compile_info_string.c_str()), reinterpret_cast<void*>(&platform_info)})
-            .Outputs({&compile_info})
-            .Build();
-
-    auto param = gert::TilingData::CreateCap(4096);
-    ASSERT_NE(param, nullptr);
-    auto workspace_size_holer = gert::ContinuousVector::Create<size_t>(4096);
-    auto ws_size = reinterpret_cast<gert::ContinuousVector*>(workspace_size_holer.get());
-    gert::StorageShape x_ref_shape = {{3, 4}, {3, 4}};
-    gert::StorageShape time_out_shape = {{1}, {1}};
-    gert::StorageShape x_ref_output_shape = {{3, 4}, {3, 4}};
-
-    std::string group("group");
-    int64_t world_size = 16;
-    auto holder = gert::TilingContextFaker()
-                        .NodeIoNum(3, 1)
-                        .IrInstanceNum({1, 1, 1})
-                        .InputShapes({&x_ref_shape, &time_out_shape, nullptr})
-                        .OutputShapes({&x_ref_output_shape})
-                        .NodeAttrs({{"group", ge::AnyValue::CreateFrom<std::string>(group)},
-                                    {"world_size", ge::AnyValue::CreateFrom<int64_t>(world_size)}})
-                        .CompileInfo(&compile_info)
-                        .PlatformInfo(reinterpret_cast<char*>(&platform_info))
-                        .NodeInputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeInputTd(1, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeInputTd(2, ge::DT_INT32, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .NodeOutputTd(0, ge::DT_FLOAT16, ge::FORMAT_ND, ge::FORMAT_ND)
-                        .TilingData(param.get())
-                        .Workspace(ws_size)
-                        .Build();
-
-    gert::TilingContext* tiling_context = holder.GetContext<gert::TilingContext>();
-    ASSERT_NE(tiling_context->GetPlatformInfo(), nullptr);
-    std::map<std::string, std::string> version = {{"Short_SoC_version", "Ascend910_93"}};
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("version", version);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
-    holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap", intrinsics);
-
-    EXPECT_EQ(tiling_func(tiling_context), ge::GRAPH_SUCCESS);
+TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_elastic_info) 
+{
+    struct DistributeBarrierCompileInfo {} compileInfo;
+    uint64_t coreNum = 20;
+    uint64_t ubSize = 196608;
+    gert::TilingContextPara tilingContextPara("DistributeBarrier",
+        {
+            {{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{}, ge::DT_INT32, ge::FORMAT_ND},
+            {{{36}, {36}}, ge::DT_INT32, ge::FORMAT_ND},
+        },
+        {{{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},},
+        {{"group", Ops::Transformer::AnyValue::CreateFrom<std::string>("group")},
+         {"world_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(16)}},
+        &compileInfo, "Ascend910_93", coreNum, ubSize);
+        uint64_t expectTilingKey = 10000UL;
+    std::string expectTilingData = "8 16 20 196352 0 256 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    uint64_t mc2TilingDataReservedLen = 42;
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces,
+                    mc2TilingDataReservedLen);
 }
+
+TEST_F(DistributeBarrierTiling, distribute_barrier_test_tiling_time_out_elastic_info) 
+{
+    struct DistributeBarrierCompileInfo {} compileInfo;
+    uint64_t coreNum = 20;
+    uint64_t ubSize = 196608;
+    gert::TilingContextPara tilingContextPara("DistributeBarrier",
+        {
+            {{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+            {{{1}, {1}}, ge::DT_INT32, ge::FORMAT_ND},
+            {{{36}, {36}}, ge::DT_INT32, ge::FORMAT_ND},
+        },
+        {{{{3, 4}, {3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},},
+        {{"group", Ops::Transformer::AnyValue::CreateFrom<std::string>("group")},
+         {"world_size", Ops::Transformer::AnyValue::CreateFrom<int64_t>(16)}},
+        &compileInfo, "Ascend910_93", coreNum, ubSize);
+        uint64_t expectTilingKey = 10000UL;
+    std::string expectTilingData = "8 16 20 196352 0 257 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    uint64_t mc2TilingDataReservedLen = 42;
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces,
+                    mc2TilingDataReservedLen);
+}
+
