7426b407dfeaa025fb26df6f1978f0b4b51748c3
skykone1 <dingfei19@huawei.com>
2025-10-22 10:35:05 +0800

    mc2 moe_distribute_dispatch和combine op kernel Ut迁移
    Co-authored-by: skykone1<dingfei19@huawei.com>
    
    !1448 merge mc2_dispatch/combine_kernel_UT into master
    
    mc2 moe_distribute_dispatch和combine op kernel Ut迁移
    
    Created-by: skykone1
    Commit-by: skykone1
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/ops-transformer-dev!1448

--- mc2/moe_distribute_combine/op_kernel/moe_distribute_combine_a2.h
+++ mc2/moe_distribute_combine/op_kernel/moe_distribute_combine_a2.h
@@ -16,8 +16,12 @@
 #include "kernel_operator.h"
 #include "kernel_tiling/kernel_tiling.h"
 #include "moe_distribute_combine_tiling.h"
+#if __has_include("../../moe_distribute_dispatch/op_kernel/moe_distribute_base.h")
+#include "../../moe_distribute_dispatch/op_kernel/moe_distribute_base.h"
+#else
 #include "../moe_distribute_dispatch/moe_distribute_base.h"
-namespace MoeDistributeCombineA2Impl {
+#endif
+namespace {
 constexpr uint8_t BUFFER_NUM = 2;                       // 多buf
 constexpr uint32_t STATE_OFFSET = 512;                  // 状态空间偏移地址
 constexpr uint32_t STATE_SPACE_SIZE = 1024 * 1024;      // 1M
