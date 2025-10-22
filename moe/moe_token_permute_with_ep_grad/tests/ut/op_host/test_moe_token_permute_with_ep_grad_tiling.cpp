4ea7f58cd5fc96fe9771c26bf131438c8fd4c075
bartonfang <fangziyang1@huawei.com>
2025-10-22 15:59:35 +0800

    finish op_host/kernel ut
    Co-authored-by: 方梓阳<fangziyang1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1432 merge MoeTokenPermuteWithEpGrad_add_all_ut into master
    
    finish op_host/kernel ut
    
    Created-by: bartonfang
    Commit-by: 方梓阳
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1432

--- moe/moe_token_permute_with_ep_grad/tests/ut/op_host/test_moe_token_permute_with_ep_grad_tiling.cpp
+++ moe/moe_token_permute_with_ep_grad/tests/ut/op_host/test_moe_token_permute_with_ep_grad_tiling.cpp
@@ -32,7 +32,7 @@ class MoeTokenPermuteWithEpGradTiling : public testing::Test {
 struct MoeTokenPermuteWithEpGradCompileInfo {
 };
 
-TEST_F(MoeTokenPermuteWithEpGradTiling, moe_token_permute_with_ep_grad_tiling_01)
+TEST_F(MoeTokenPermuteWithEpGradTiling, test_tiling_bf16)
 {
     MoeTokenPermuteWithEpGradCompileInfo compileInfo = {}; // 根据tiling头文件中的compileInfo填入对应值，一般原先的用例里能找到
     std::vector<int64_t> range({1, 49152});
@@ -58,9 +58,83 @@ TEST_F(MoeTokenPermuteWithEpGradTiling, moe_token_permute_with_ep_grad_tiling_01
                                             },
                                             // attr
                                             {
-                                                {"num_topk", ge::AnyValue::CreateFrom<int64_t>(8)},
-                                                {"range", ge::AnyValue::CreateFrom<std::vector<int64_t>>(range)},
-                                                {"padded_mode", ge::AnyValue::CreateFrom<bool>(false)}
+                                                {"num_topk", Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"range", Ops::Transformer::AnyValue::CreateFrom<std::vector<int64_t>>(range)},
+                                                {"padded_mode", Ops::Transformer::AnyValue::CreateFrom<bool>(false)}
+                                            },
+                                            &compileInfo);
+    int64_t expectTilingKey = 5; // tilngkey
+    string expectTilingData = "1 2 5 16 3 1 6 1 6 6 6 1 6 6 8160 0 0 1024 2 2 0 1 0 10 2 2 2340 4680 14040 1 1 1 1 1 0 4680 2 2340 1 1 4 1 5 74880 14048 14048 ";
+    std::vector<size_t> expectWorkspaces = {16777376}; // workspace
+}
+
+
+TEST_F(MoeTokenPermuteWithEpGradTiling, test_tiling_fp16)
+{
+    MoeTokenPermuteWithEpGradCompileInfo compileInfo = {}; // 根据tiling头文件中的compileInfo填入对应值，一般原先的用例里能找到
+    std::vector<int64_t> range({1, 49152});
+    // input
+    gert::StorageShape permuted_token_output_d_shape = {{range[1] - range[0], 5120}, {range[1] - range[0], 5120}};
+    gert::StorageShape sorted_indices_shape = {{49152}, {49152}};
+    gert::StorageShape permuted_probs_output_d_shape = {{range[1] - range[0]}, {range[1] - range[0]}};
+    // output
+    gert::StorageShape input_token_grad_shape = {{6144, 5120}, {6144, 5120}};
+    gert::StorageShape input_probs_grad_shape = {{6144, 8}, {6144, 8}};
+    gert::TilingContextPara tilingContextPara("MoeTokenPermuteWithEpGrad", // op_name
+                                            {
+                                            // input info
+                                            // shape都需要重复一次，比如shape为{16,16}，要填入{{16, 16}, {16, 16}}
+                                                {permuted_token_output_d_shape, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {sorted_indices_shape, ge::DT_INT32, ge::FORMAT_ND},
+                                                {permuted_probs_output_d_shape, ge::DT_FLOAT16, ge::FORMAT_ND}
+                                            },
+                                            // output info
+                                            {
+                                                {input_token_grad_shape, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                {input_probs_grad_shape, ge::DT_FLOAT16, ge::FORMAT_ND}
+                                            },
+                                            // attr
+                                            {
+                                                {"num_topk", Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"range", Ops::Transformer::AnyValue::CreateFrom<std::vector<int64_t>>(range)},
+                                                {"padded_mode", Ops::Transformer::AnyValue::CreateFrom<bool>(false)}
+                                            },
+                                            &compileInfo);
+    int64_t expectTilingKey = 5; // tilngkey
+    string expectTilingData = "1 2 5 16 3 1 6 1 6 6 6 1 6 6 8160 0 0 1024 2 2 0 1 0 10 2 2 2340 4680 14040 1 1 1 1 1 0 4680 2 2340 1 1 4 1 5 74880 14048 14048 ";
+    std::vector<size_t> expectWorkspaces = {16777376}; // workspace
+}
+
+
+TEST_F(MoeTokenPermuteWithEpGradTiling, test_tiling_fp32)
+{
+    MoeTokenPermuteWithEpGradCompileInfo compileInfo = {}; // 根据tiling头文件中的compileInfo填入对应值，一般原先的用例里能找到
+    std::vector<int64_t> range({1, 49152});
+    // input
+    gert::StorageShape permuted_token_output_d_shape = {{range[1] - range[0], 5120}, {range[1] - range[0], 5120}};
+    gert::StorageShape sorted_indices_shape = {{49152}, {49152}};
+    gert::StorageShape permuted_probs_output_d_shape = {{range[1] - range[0]}, {range[1] - range[0]}};
+    // output
+    gert::StorageShape input_token_grad_shape = {{6144, 5120}, {6144, 5120}};
+    gert::StorageShape input_probs_grad_shape = {{6144, 8}, {6144, 8}};
+    gert::TilingContextPara tilingContextPara("MoeTokenPermuteWithEpGrad", // op_name
+                                            {
+                                            // input info
+                                            // shape都需要重复一次，比如shape为{16,16}，要填入{{16, 16}, {16, 16}}
+                                                {permuted_token_output_d_shape, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {sorted_indices_shape, ge::DT_INT32, ge::FORMAT_ND},
+                                                {permuted_probs_output_d_shape, ge::DT_FLOAT, ge::FORMAT_ND}
+                                            },
+                                            // output info
+                                            {
+                                                {input_token_grad_shape, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                {input_probs_grad_shape, ge::DT_FLOAT, ge::FORMAT_ND}
+                                            },
+                                            // attr
+                                            {
+                                                {"num_topk", Ops::Transformer::AnyValue::CreateFrom<int64_t>(8)},
+                                                {"range", Ops::Transformer::AnyValue::CreateFrom<std::vector<int64_t>>(range)},
+                                                {"padded_mode", Ops::Transformer::AnyValue::CreateFrom<bool>(false)}
                                             },
                                             &compileInfo);
     int64_t expectTilingKey = 5; // tilngkey
