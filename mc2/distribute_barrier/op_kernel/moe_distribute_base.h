355ac641f280723cef68fa8e9f48cfb83d6171ea
macech <chenluchun1@huawei.com>
2025-10-28 10:35:44 +0800

    修改Dispatch/Barrier LOCAL_STREAM_MAX_NUM为19

--- mc2/distribute_barrier/op_kernel/moe_distribute_base.h
+++ mc2/distribute_barrier/op_kernel/moe_distribute_base.h
@@ -19,7 +19,7 @@
 #include "kernel_operator.h"
 
 constexpr uint32_t LOCAL_NOTIFY_MAX_NUM = 64;
-constexpr uint32_t LOCAL_STREAM_MAX_NUM = 40U;
+constexpr uint32_t LOCAL_STREAM_MAX_NUM = 19U;
 constexpr uint32_t AICPU_OP_NOTIFY_MAX_NUM = 2;
 constexpr uint32_t AICPU_MAX_RANK_NUM = 128 * 1024;
 
