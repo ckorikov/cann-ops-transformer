636c6c5dc5a04eaf969d96438a24a5677d63f139
ClarkXie <xiezhengkai@huawei.com>
2025-10-29 10:37:51 +0800

    IFA支持aclnn fallback两阶段入图
    Co-authored-by: 谢正恺<xiezhengkai@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1644 merge IFA_aclnn_two_stages into master
    
    IFA支持aclnn fallback两阶段入图
    
    Created-by: ClarkXie
    Commit-by: 谢正恺
    Merged-by: turing_project1
    Description: ## 描述
    IFA适配支持aclnn fallback两阶段入图，用于动态shape性能优化，搭配多线程执行器后通过多级流水获得收益。
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1644

--- attention/incre_flash_attention/op_host/fallback_incre_flash_attention.cpp
+++ attention/incre_flash_attention/op_host/fallback_incre_flash_attention.cpp
@@ -9,8 +9,8 @@
  */
 
 #include <vector>
-#include "fallback/fallback_comm.h"
-#include "fallback/fallback.h"
+#include "fallback/fallback_comm_2stages.h"
+#include "fallback/fallback_2stages.h"
 #include "log/log.h"
 #include "log/error_code.h"
 
@@ -43,7 +43,7 @@ static const size_t KV_HEAD_NUM_INDEX = 3;
 static const size_t BLOCK_SIZE_INDEX = 4;
 static const size_t INNER_PRECISE_INDEX = 5;
 
-graphStatus IncreHostExecuteFunc(OpExecuteContext *host_api_ctx)
+graphStatus IncreHostExecuteFunc(OpExecutePrepareContext *host_api_ctx)
 {
     OP_CHECK_IF(host_api_ctx == nullptr, OP_LOGE("aclnnfallback", "host_api_ctx is null"), return GRAPH_FAILED);
 
@@ -113,7 +113,7 @@ graphStatus IncreHostExecuteFunc(OpExecuteContext *host_api_ctx)
 
     // execute opapi
     auto api_ret =
-        EXEC_OPAPI_CMD(aclnnIncreFlashAttentionV4, query, ge_tenserListKey, ge_tenserListValue, pseShiftGe, attenMaskGe,
+        EXEC_OPAPI_PREPARE_CMD(aclnnIncreFlashAttentionV4, query, ge_tenserListKey, ge_tenserListValue, pseShiftGe, attenMaskGe,
                        actSeqArray, dequantScale1Ge, quantScale1Ge, dequantScale2Ge, quantScale2Ge, quantOffset2Ge,
                        antiquantScaleGe, antiquantOffsetGe, blocktableGe, kvPaddingSizeGe, *num_heads, dScaleValue,
                        layout, *kvHeadNum, *blockSize, *innerPrecise, output);
@@ -122,7 +122,7 @@ graphStatus IncreHostExecuteFunc(OpExecuteContext *host_api_ctx)
     return GRAPH_SUCCESS;
 }
 
-IMPL_OP(IncreFlashAttention).OpExecuteFunc(IncreHostExecuteFunc).HostInputs({5});
+IMPL_OP(IncreFlashAttention).Op2StageExecuteFuncs(IncreHostExecuteFunc, ExecuteOpLaunch).HostInputs({5});
 
 } // namespace fallback
 
