04b04972e5d83c0c9d170a6fc8103492d9290aaa
lyt_claire <luyitong1@huawei.com>
2025-10-29 20:40:29 +0800

    all_gather_add v1.0

---
+++ mc2/all_gather_add/op_host/all_gather_add_def.cpp
@@ -0,0 +1,63 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file all_gather_add_def.cpp
+ * \brief
+ */
+#include "register/op_def_registry.h"
+
+namespace ops {
+class AllGatherAdd : public OpDef {
+ public:
+  explicit AllGatherAdd(const char *name) : OpDef(name) {
+    this->Input("a")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+    this->Input("b")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND})
+        .IgnoreContiguous();
+    
+    this->Output("c")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+    this->Output("gather_out")
+        .ParamType(REQUIRED)
+        .DataType({ge::DT_FLOAT16, ge::DT_BF16})
+        .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+        .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+
+    this->Attr("group").AttrType(REQUIRED).String(); // 通算融合算子属性，表示通信域名称
+    this->Attr("rank_size").AttrType(OPTIONAL).Int(0); // ?
+    this->Attr("is_gather_out").AttrType(OPTIONAL).Bool(true);
+
+    this->AICore().SetTiling(optiling::AllGatherAddTilingFunc);
+    OpAICoreConfig aicore_config;
+    aicoreConfig.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(false)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false)
+            .PrecisionReduceFlag(true)
+            .ExtendCfgInfo("opFile.value", "all_gather_add");    // 这里制定的值会对应到kernel入口文件名.cpp
+    this->AICore().AddConfig("ascend910b", aicore_config);
+    this->MC2().HcclGroup("group"); // group 属性配置为该算子的通信域名称
+  }
+};
+
+OP_ADD(AllGatherAdd);
+}  // namespace ops
---
+++ mc2/all_gather_add/op_host/op_tiling/all_gather_add_tiling.cpp
@@ -0,0 +1,109 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file all_gather_add_tiling.cc
+ * \brief
+ */
+#include "vector"
+#include "mc2_hcom_topo_info.h"
+#include "mc2_log.h"
+#include "ops_utils.h"
+#include "graph/utils/type_utils.h"
+#include "register/op_def_registry.h"
+#include "tiling/mc2_tiling_utils.h"
+#include "tiling/matmul_formulaic_tiling.h"
+#include "../../op_kernel/all_gather_add_tiling.h"
+
+using namespace AscendC;
+using namespace ge;
+using namespace Ops::Math::OpTiling;
+
+constexpr uint32_t TILE_LENGTH = 128; // 先随手
+
+namespace optiling {
+
+static ge::graphStatus AllGatherParamsCheck(const gert::TilingContext* context)
+{
+    OP_TILING_CHECK(mc2tiling::Mc2TilingUtils::CommonParamCheck(context) != ge::GRAPH_SUCCESS,
+        VECTOR_INNER_ERR_REPORT_TILING(context->GetNodeName(), "common check failed"), return ge::GRAPH_FAILED);
+
+    const gert::StorageShape* aShape = context->GetInputShape(0);
+    uint64_t valueOne = aShape->GetStorageShape().GetDim(0);
+    uint64_t valueTwo = aShape->GetStorageShape().GetDim(1);
+
+    OP_TILING_CHECK(valueOne == 0 || valueTwo == 0,
+        VECTOR_INNER_ERR_REPORT_TILING(context->GetNodeName(), "the value is invalid"), return ge::GRAPH_FAILED);
+    
+    if (context->GetAttrs() == nullptr) {
+        VECTOR_INNER_ERR_REPORT_TILING(context->GetNodeName(), "get attrs failed");
+    } 
+    auto group = context->GetAttrs()->GetAttrPointer<char>(static_cast<int>(0));
+    OP_TILING_CHECK(group == nullptr, VECTOR_INNER_ERR_REPORT_TILING(context->GetNodeName(), "group is nullptr. "),
+                    return ge::GRAPH_FAILED);
+
+    return ge::GRAPH_SUCCESS;
+}
+
+// 设置hccl高阶API Tiling结构体
+static void InitHcclParam(AllGatherAddTilingData* tilingData, const char* group)
+{
+
+    std::string algConfig = "AllGather=level0:doublering"; // 啥
+    Mc2CcTilingConfig mc2CcTilingConfig(group, HCCL_CMD_ALLGATHER, algConfig, HCCL_REDUCE_SUM);
+    mc2CcTilingConfig.SetSkipBufferWindowCopy(0); // 需要输出本卡通信算法的计算结果 ？
+    mc2CcTilingConfig.GetTiling(tilingData->mc2InitTiling);
+    mc2CcTilingConfig.GetTiling(tilingData->mc2CcTiling);
+}
+
+static ge::graphStatus AllGatherAddTilingFunc(gert::TilingContext *context) {
+    // 对参数进行校验
+    OP_TILING_CHECK(AllGatherParamsCheck(context) != ge::GRAPH_SUCCESS,
+                    VECTOR_INNER_ERR_REPORT_TILING(context->GetNodeName(), "param is invalid"), return ge::GRAPH_FAILED);
+    
+    // 设置kernel核数 SetBlockDim 2 4 8
+    auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+    context->SetBlockDim(ascendcPlatform.GetCoreNumAiv());
+
+    // 设置TilingData
+    AllGatherAddTilingData* tilingData = context->GetTilingData<AllGatherAddTilingData>();
+    OP_CHECK_NULL_WITH_CONTEXT(context, tilingData);
+    OP_CHECK_IF(
+        memset_s(tilingData, sizeof(AllGatherAddTilingData), 0, sizeof(AllGatherAddTilingData)) != EOK,
+        OP_LOGE(context, "set AllGatherAdd tiling data error"), return ge::GRAPH_FAILED);
+    tilingData->cfg.totalLength = context->GetInputTensor(0)->GetShapeSize();
+    tilingData->cfg.tileLength = TILE_LENGTH;
+    tilingData->cfg.tileNum = tilingData->cfg.totalLength / tilingData->cfg.tileLength;
+
+    // 设置workspaceSize gather out需要额外的临时内存，大小=input b
+    size_t* currentWorkspace = context->GetWorkspaceSizes(1);
+    OP_CHECK_NULL_WITH_CONTEXT(context,currentWorkspace);
+    // 如需使用系统workspace需要调用GetLibApiWorkSpaceSize获取系统workspace大小
+    uint32_t sysWorkSpaceSize = ascendcPlatform.GetLibApiWorkSpaceSize(); 
+    // 预留18M + gather_out, gather_out 大小跟x1输入一样
+    currentWorkspace[0] = sysWorkSpaceSize + tilingData->cfg.totalLength;
+
+    auto group = context->GetAttrs()->GetAttrPointer<char>(static_cast<int>(0));
+    InitHcclParam(tilingData, group);
+    return ge::GRAPH_SUCCESS;
+}
+
+struct AllGatherMatmulCompileInfo {};
+
+static ge::graphStatus TilingParseForAllGatherAdd([[maybe_unused]] gert::TilingParseContext *context)
+{ 
+    (void)context;
+    return ge::GRAPH_SUCCESS;
+}
+
+IMPL_OP_OPTILING(AllGatherMatmul)
+    .Tiling(AllGatherAddTilingFunc)
+    .TilingParse<AllGatherMatmulCompileInfo>(TilingParseForAllGatherAdd);
+}  // namespace optiling
