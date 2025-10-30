02a1ea71c92158603ba1101d28ee66dc08bbbc0d
liujiawei-21 <liujiawei56@huawei.com>
2025-10-30 09:51:22 +0800

    非量化拦截校验修复
    Co-authored-by: liujiawei-21<liujiawei56@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1701 merge master into master
    
    非量化拦截校验修复
    
    Created-by: liujiawei-21
    Commit-by: liujiawei-21
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1701

--- attention/prompt_flash_attention/regbase/ophost/prompt_flash_attention_tiling_v2.cpp
+++ attention/prompt_flash_attention/regbase/ophost/prompt_flash_attention_tiling_v2.cpp
@@ -584,6 +584,16 @@ bool PromptFlashAttentionTilingV2::CheckInputDimAndHeadNum(ContextParamsForPFATi
                 queryDim, keyDim, valueDim);
             return false;
         }
+    } else if ((inputLayout == InputLayout::TND) && (!enablePA)) {
+        if ((queryDim == 3) && (keyDim == 3) && (valueDim == 3)) { // dim num: 3
+            queryShapeHeadNum = queryShape->GetStorageShape().GetDim(nIdx);
+            keyShapeHeadNum = keyShape->GetStorageShape().GetDim(nIdx);
+            valueShapeHeadNum = valueShape->GetStorageShape().GetDim(nIdx);
+        } else {
+            OP_LOGE(contextKeyParams.opName, "input dim of q(%zu), k(%zu), v(%zu) must be 3 for TND format!",
+                queryDim, keyDim, valueDim);
+            return false;
+        }
     }
 
     OP_CHECK_IF(nQ > 256U, // The maximum limit for head is 256.
@@ -1338,7 +1348,11 @@ bool PromptFlashAttentionTilingV2::CheckQueryAndKey(ContextParamsForPFATiling& c
         (!enableTensorList) && (!enablePA), OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
         "query batch must be equal to key/value batch, query batch = %u , key/value batch = %u.",
         queryShapeInfo.b, keyShapeInfo.b), return false);
-
+    // check d size
+    OP_CHECK_IF((contextKeyParams.fromTilingSink == 0) && (queryShapeInfo.d != keyShapeInfo.d) &&
+        (!enableTensorList) && (!enablePA), OPS_REPORT_VECTOR_INNER_ERR(contextKeyParams.opName,
+        "query d size must be equal to key/value d size, query d = %u , key/value d = %u.",
+        queryShapeInfo.d, keyShapeInfo.d), return false);
     return true;
 }
 
