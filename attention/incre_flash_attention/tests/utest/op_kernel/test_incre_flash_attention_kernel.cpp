e559406a7b2459860cfa6bbe824341b48ac8eee0
wangsong96 <wangsong109@huawei.com>
2025-11-01 16:58:50 +0800

    修改推理FA资料资料描述，并移除不相关UT用例

--- attention/incre_flash_attention/tests/utest/op_kernel/test_incre_flash_attention_kernel.cpp
+++ attention/incre_flash_attention/tests/utest/op_kernel/test_incre_flash_attention_kernel.cpp
@@ -1,7 +1,19 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+/*!
+ * \file test_incre_flash_attention_kernel.cpp
+ * \brief IncreFlashAttention用例.
+ */
+
 #include "ts_ifa.h"
 class Ts_Ifa_Ascend910B2_Case : public Ts_Ifa_WithParam_Ascend910B2 {};
-class Ts_Ifa_Ascend310P3_Case : public Ts_Ifa_WithParam_Ascend310P3 {};
-class Ts_Ifa_Ascend910_9591_Case : public Ts_Ifa_WithParam_Ascend910_9591 {};
 
 TEST_P(Ts_Ifa_Ascend910B2_Case, general_case)
 {
