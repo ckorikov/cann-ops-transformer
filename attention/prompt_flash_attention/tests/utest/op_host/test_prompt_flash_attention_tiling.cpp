ce1289f0eb09fc62866344301d176f457e2f1549
turing_project1 <songchangxia@cann.team>
2025-11-03 20:30:15 +0800

    整改PFA的tiling和kernel UT用例
    Co-authored-by: lilongwei_hw<lilongwei11@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1839 merge master into master
    
    整改PFA的tiling和kernel UT用例
    
    Created-by: lilongwei_hw
    Commit-by: lilongwei_hw
    Merged-by: turing_project1
    Description: ## 描述
    整改PFA的tiling和kernel UT用例
    
    ## 关联的Issue
    关联Issue [#83](https://gitcode.com/cann/ops-transformer-dev/issues/83)
    
    
    ## 测试
    UT测试/前冒烟测试/耳机冒烟测试
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1839

--- attention/prompt_flash_attention/tests/utest/op_host/test_prompt_flash_attention_tiling.cpp
+++ attention/prompt_flash_attention/tests/utest/op_host/test_prompt_flash_attention_tiling.cpp
@@ -17,31 +17,7 @@
 class Ts_Pfa_Ascend910B2_Case : public Ts_Pfa_WithParam_Ascend910B2 {};
 class Ts_Pfa_Ascend310P3_Case : public Ts_Pfa_WithParam_Ascend310P3 {};
 class Ts_Pfa_Ascend910_9591_Case : public Ts_Pfa_WithParam_Ascend910_9591 {};
-TEST_P(Ts_Pfa_Ascend910B2_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
-TEST_P(Ts_Pfa_Ascend310P3_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
-TEST_P(Ts_Pfa_Ascend910_9591_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
 
-const auto Tc_Pfa_General_Case =
-    ::testing::Values(PfaCase("case_001", true, "",
-                              OpInfo(ControlInfo(true, false), ExpectInfo(true, ExpectInfo::kInvalidTilingKey,
-                                                                          ExpectInfo::kInvalidTilingBlockDim)),
-                              PfaCase::Param(1, 4, 1024, 128, "BSH", 4, 4, 1.0f, 0, 1, 0, 524288, 0)));
-
-INSTANTIATE_TEST_SUITE_P(Pfa, Ts_Pfa_Ascend910B2_Case, Tc_Pfa_General_Case);
-INSTANTIATE_TEST_SUITE_P(Pfa, Ts_Pfa_Ascend310P3_Case, Tc_Pfa_General_Case);
-INSTANTIATE_TEST_SUITE_P(Pfa, Ts_Pfa_Ascend910_9591_Case, Tc_Pfa_General_Case);
 
 TEST_F(Ts_Pfa_Ascend910B2, case_empty_query)
 {
