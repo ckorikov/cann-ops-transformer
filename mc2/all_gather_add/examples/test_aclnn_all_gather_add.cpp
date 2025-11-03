ae10b01189f18c4013dd5d953de744b85066657b
lyt_claire <luyitong1@huawei.com>
2025-11-03 21:18:57 +0800

    aclnn接口捉虫

--- mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
+++ mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
@@ -76,7 +76,7 @@ int launchOneThread_AllGatherAdd(Args &args)
     LOG_PRINT("[INFO] rank = %d, hcomName = %s, stream = %p\n", args.rankId, hcomName, args.stream);
     std::vector<int64_t> aShape = {16, 256};
     std::vector<int64_t> gatherOutShape = {16 * RANK_DIM, 256};
-    std::vector<int64_t> bShape = {32, 256};
+    std::vector<int64_t> bShape = {16 * RANK_DIM, 256};
     std::vector<int64_t> outputShape = {16 * RANK_DIM, 256};
     void *aDeviceAddr = nullptr;
     void *bDeviceAddr = nullptr;
