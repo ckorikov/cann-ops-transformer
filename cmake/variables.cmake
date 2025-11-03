20b42e1c526a2603ac3fcf85c91375aef89659d2
huang-chuhong <huangchuhong1@h-partners.com>
2025-11-03 09:42:07 +0800

    replace opbase include
    Co-authored-by: huang-chuhong<huangchuhong1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1709 merge master into master
    
    replace opbase include
    
    Created-by: huang-chuhong
    Commit-by: huang-chuhong
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1709

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -130,7 +130,8 @@ if (NOT BUILD_OPEN_PROJECT)
     ${TOP_DIR}/ace/npuruntime/acl/inc/external/acl/error_codes
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_tiling/runtime
     ${TOP_DIR}/asl/ops/cann/ops/built-in
-    ${TOP_DIR}/ops-base/include/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc
     ${TOP_DIR}/ops-base/include
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_fallback
   )
@@ -181,7 +182,8 @@ if (NOT BUILD_OPEN_PROJECT)
     ${TOP_DIR}/asl/ops/cann/ops/built-in
 
     ${TOP_DIR}/asl/ops/cann/ops/mc2/communication_and_computation
-    ${TOP_DIR}/ops-base/include/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc
     ${TOP_DIR}/ops-base/include
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_fallback
 
@@ -230,7 +232,8 @@ if (NOT BUILD_OPEN_PROJECT)
     ${TOP_DIR}/ace/npuruntime/acl/inc/external/acl/error_codes
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_tiling/runtime
     ${TOP_DIR}/asl/ops/cann/ops/built-in
-    ${TOP_DIR}/ops-base/include/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc/op_common/op_host
+    ${TOP_DIR}/ops-base/pkg_inc
     ${TOP_DIR}/ops-base/include
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_fallback
   )
@@ -246,6 +249,7 @@ if (NOT BUILD_OPEN_PROJECT)
     ${TOP_DIR}/asl/ops/cann/ops/built-in/aicpu/context/common/*.h
     ${TOP_DIR}/ace/comop/inc
     ${TOP_DIR}/ops-base/include
+    ${TOP_DIR}/ops-base/pkg_inc
     ${TOP_DIR}/asl/ops/cann/ops/built-in/op_fallback
   )
 endif()
