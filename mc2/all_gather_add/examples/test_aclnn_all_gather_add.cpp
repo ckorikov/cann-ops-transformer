d2f307089b8b3b02af2a97834c9884f4232c6d89
lyt_claire <luyitong1@huawei.com>
2025-11-03 10:46:53 +0800

    添加aclnn

---
+++ mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
@@ -0,0 +1,207 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_aclnn_all_gather_matmul.cpp
+ * \brief
+ */
+
+#include <thread>
+#include <iostream>
+#include <vector>
+#include "../op_host/op_api/aclnn_all_gather_add.h"
+
+#define CHECK_RET(cond, return_expr) \
+    do {                             \
+        if (!(cond)) {               \
+            return_expr;             \
+        }                            \
+    } while (0)
+
+#define LOG_PRINT(message, ...)         \
+    do {                                \
+        printf(message, ##__VA_ARGS__); \
+    } while(0)
+
+constexpr int RANK_DIM = 8;
+
+int64_t GetShapeSize(const std::vector<int64_t> &shape)
+{
+    int64_t shape_size = 1;
+    for (auto i : shape) {
+        shape_size *= i;
+    }
+    return shape_size;
+}
+
+template<typename T>
+int CreateAclTensor(const std::vector<T> &hostData, const std::vector<int64_t> &shape, void **deviceAddr,
+    aclDataType dataType, aclTensor **tensor)
+{
+    auto size = GetShapeSize(shape) * sizeof(T);
+    auto ret = aclrtMalloc(deviceAddr, size, ACL_MEM_MALLOC_HUGE_FIRST);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtMalloc failed. ret: %d\n", ret); return ret);
+    ret = aclrtMemcpy(*deviceAddr, size, hostData.data(), size, ACL_MEMCPY_HOST_TO_DEVICE);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtMemcpy failed. ret: %d\n", ret); return ret);
+    std::vector<int64_t> strides(shape.size(), 1);
+    for (int64_t i = shape.size() - 2; i >= 0; i--) {
+        strides[i] = shape[i +1] * strides[i + 1];
+    }
+    *tensor = aclCreateTensor(shape.data(), shape.size(), dataType, strides.data(), 0, aclFormat::ACL_FORMAT_ND,
+        shape.data(), shape.size(), *deviceAddr);
+    return 0;
+}
+
+struct Args {
+    int rankId;
+    HcclComm hcclComm;
+    aclrtStream stream;
+  };
+
+int launchOneThread_AllGatherAdd(Args &args)
+{
+    int ret = aclrtSetDevice(args.rankId);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtSetDevice failed. ret = %d \n", ret); return ret);
+
+    char hcomName[128] = {0};
+    ret = HcclGetCommName(args.hcclComm, hcomName);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] HcclGetCommName failed. ret: %d\n", ret); return -1);
+    LOG_PRINT("[INFO] rank = %d, hcomName = %s, stream = %p\n", args.rankId, hcomName, args.stream);
+    std::vector<int64_t> aShape = {16, 256};
+    std::vector<int64_t> gatherOutShape = {16 * RANK_DIM, 256};
+    std::vector<int64_t> bShape = {32, 256};
+    std::vector<int64_t> outputShape = {16 * RANK_DIM, 256};
+    void *aDeviceAddr = nullptr;
+    void *bDeviceAddr = nullptr;
+    void *outDeviceAddr = nullptr;
+    void *gatherOutDeviceAddr = nullptr;
+    aclTensor *a = nullptr;
+    aclTensor *b = nullptr;
+    aclTensor *out = nullptr;
+    aclTensor *gatherOut = nullptr;
+
+    uint64_t workspaceSize = 0;
+    aclOpExecutor *executor = nullptr;
+    void *workspaceAddr = nullptr;
+
+    long long aShapeSize = GetShapeSize(aShape);
+    long long bShapeSize = GetShapeSize(bShape);
+    long long outShapeSize = GetShapeSize(outputShape);
+    long long gatherOutShapeSize = GetShapeSize(gatherOutShape);
+
+    std::vector<int16_t> aHostData(aShapeSize, 0);
+    std::vector<int16_t> bHostData(bShapeSize, 0);
+    std::vector<int16_t> outHostData(outShapeSize, 0);
+    std::vector<int16_t> gatherOutHostData(gatherOutShapeSize, 0);
+
+    ret = CreateAclTensor(aDeviceAddr, aShapeSize, &aDeviceAddr, aclDataType::ACL_FLOAT16, &a);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    ret = CreateAclTensor(bHostData, bShape, &bDeviceAddr, aclDataType::ACL_FLOAT16, &b);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    ret = CreateAclTensor(outHostData, outputShape, &outDeviceAddr, aclDataType::ACL_FLOAT16, &out);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+    ret = CreateAclTensor(gatherOutHostData, gatherOutShape, &gatherOutDeviceAddr,
+        aclDataType::ACL_FLOAT16, &gatherOut);
+    CHECK_RET(ret == ACL_SUCCESS, return ret);
+
+    // 调用第一阶段接口
+    ret = aclnnAllGatherAddGetWorkspaceSize(
+        a, b, hcomName, out, gatherOut, &workspaceSize, &executor);
+    CHECK_RET(ret == ACL_SUCCESS,
+        LOG_PRINT("[ERROR] aclnnAllGatherAddGetWorkspaceSize failed. ret = %d \n", ret); return ret);
+    // 根据第一阶段接口计算出的workspaceSize申请device内存
+    if (workspaceSize > 0) {
+        ret = aclrtMalloc(&workspaceAddr, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtMalloc workspace failed. ret = %d \n", ret);  return ret);
+    }
+    // 调用第二阶段接口
+    ret = aclnnAllGatherAdd(workspaceAddr, workspaceSize, executor, args.stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclnnAllGatherAdd failed. ret = %d \n", ret); return    ret);
+    // （固定写法）同步等待任务执行结束
+    ret = aclrtSynchronizeStreamWithTimeout(args.stream, 10000);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtSynchronizeStreamWithTimeout failed. ret = %d \n",    ret);
+        return ret);
+    LOG_PRINT("[INFO] device_%d aclnnAllGatherAdd execute successfully.\n", args.rankId);
+    // 释放device资源，需要根据具体API的接口定义修改
+    if (x1 != nullptr) {
+        aclDestroyTensor(a);
+    }
+    if (x2 != nullptr) {
+        aclDestroyTensor(x2);
+    }
+    if (out != nullptr) {
+        aclDestroyTensor(out);
+    }
+    if (gatherOut != nullptr) {
+        aclDestroyTensor(gatherOut);
+    }
+    if (aDeviceAddr != nullptr) {
+        aclrtFree(aDeviceAddr);
+    }
+    if (bDeviceAddr != nullptr) {
+        aclrtFree(bDeviceAddr);
+    }
+    if (outDeviceAddr != nullptr) {
+        aclrtFree(outDeviceAddr);
+    }
+    if (gatherOutDeviceAddr != nullptr) {
+        aclrtFree(gatherOutDeviceAddr);
+    }
+    if (workspaceSize > 0) {
+        aclrtFree(workspaceAddr);
+    }
+    ret = aclrtDestroyStream(args.stream);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtDestroyStream failed. ret = %d \n", ret); return ret);
+    ret = aclrtResetDevice(args.rankId);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtResetDevice failed. ret = %d \n", ret); return ret);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // 本样例基于Atlas A3实现，必须在Atlas A3上运行
+    // AscendCL初始化
+    int ret = aclInit(nullptr);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclInit failed. ret = %d \n", ret); return ret);
+    aclrtStream stream[RANK_DIM];
+    for (uint32_t rankId = 0; rankId < RANK_DIM; rankId++) {
+        ret = aclrtSetDevice(rankId);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtSetDevice failed. ret = %d \n", ret); return ret);
+        ret = aclrtCreateStream(&stream[rankId]);
+        CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] aclrtCreateStream failed. ret = %d \n", ret); return   ret);
+    }
+    int32_t devices[RANK_DIM];
+    for (int i = 0; i < RANK_DIM; i++) {
+        devices[i] = i;
+    }
+    // 初始化集合通信域
+    HcclComm comms[RANK_DIM];
+    ret = HcclCommInitAll(RANK_DIM, devices, comms);
+    CHECK_RET(ret == ACL_SUCCESS, LOG_PRINT("[ERROR] HcclCommInitAll failed. ret = %d \n", ret); return ret);
+
+    Args args[RANK_DIM];
+    // 启动多线程
+    std::vector<std::unique_ptr<std::thread>> threads(RANK_DIM);
+    for (uint32_t rankId = 0; rankId < RANK_DIM; rankId++) {
+        args[rankId].rankId = rankId;
+        args[rankId].hcclComm = comms[rankId];
+        args[rankId].stream = stream[rankId];
+        threads[rankId].reset(new(std::nothrow) std::thread(&launchOneThread_AllGatherAdd, std::ref(args [rankId])));    
+    }
+    for (uint32_t rankId = 0; rankId < RANK_DIM; rankId++) {
+        threads[rankId]->join();
+    }
+    for (int i = 0; i < RANK_DIM; i++) {
+        auto hcclRet = HcclCommDestroy(comms[i]);
+        CHECK_RET(hcclRet == HCCL_SUCCESS, LOG_PRINT("[ERROR] HcclCommDestroy failed. ret = %d \n", ret); return    -1);
+    }
+    aclFinalize();
+    return 0;
+}
