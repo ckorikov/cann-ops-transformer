11bb33dcd3737a991ad3d4e89ca8bfb07ee4a326
wangsong96 <wangsong109@huawei.com>
2025-11-03 16:06:58 +0800

    整改IFA的tiling和kernel UT用例
    Co-authored-by: wangsong96<wangsong109@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1835 merge master into master
    
    整改IFA的tiling和kernel UT用例
    
    Created-by: wangsong96
    Commit-by: wangsong96
    Merged-by: turing_project1
    Description: ## 描述
    修改IncreFlashAttention算子的tiling的UT用例，并增加kernel的UT用例
    
    ## 关联的Issue
    关联Issue [#82](https://gitcode.com/cann/ops-transformer-dev/issues/82) 
    
    ## 测试
    UT测试/前冒烟测试/二级冒烟测试
    
    ## 文档更新
    无
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!1835

--- attention/incre_flash_attention/tests/utest/op_host/test_incre_flash_attention_tiling.cpp
+++ attention/incre_flash_attention/tests/utest/op_host/test_incre_flash_attention_tiling.cpp
@@ -16,66 +16,6 @@
 class Ts_Ifa_Ascend910B2_Case : public Ts_Ifa_WithParam_Ascend910B2 {};
 class Ts_Ifa_Ascend310P3_Case : public Ts_Ifa_WithParam_Ascend310P3 {};
 class Ts_Ifa_Ascend910_9591_Case : public Ts_Ifa_WithParam_Ascend910_9591 {};
-TEST_P(Ts_Ifa_Ascend910B2_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
-TEST_P(Ts_Ifa_Ascend310P3_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
-TEST_P(Ts_Ifa_Ascend910_9591_Case, general_case)
-{
-    ASSERT_TRUE(case_->Init());
-    ASSERT_TRUE(case_->Run());
-}
-
-const auto Tc_Ifa_General_Case = ::testing::Values(
-    IfaCase("case_001", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(1, 4, 1024, 128, "BSH", 4, 4, 1.0f, 0, 1, {})),
-    IfaCase("case_002", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(1, 5, 128, 128, "BSH", 5, 5, 1.0f, 0, 1, {})),
-    IfaCase("case_003", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(1, 40, 128, 128, "BSH", 40, 40, 1.0f, 0, 1, {})),
-    IfaCase("case_004", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(13, 20, 2048, 128, "BSH", 20, 20, 1.0f, 0, 1, {})),
-    IfaCase("case_005", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(20, 40, 2048, 128, "BSH", 40, 40, 1.0f, 0, 1, {})),
-    IfaCase("case_006", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(20, 40, 2048, 128, "BSH", 40, 40, 1.0f, 0, 1,
-                           {1024, 512,  2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048,
-                            2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048})),
-    IfaCase("case_007", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(1, 40, 128, 128, "BNSD", 40, 40, 1.0f, 0, 1, {})),
-    IfaCase("case_008", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(1, 40, 1, 128, "BSH", 40, 40, 1.0f, 0, 1, {})),
-    IfaCase("case_009", true, "dbginfo",
-            OpInfo(ControlInfo(true, false),
-                   ExpectInfo(true, ExpectInfo::kInvalidTilingKey, ExpectInfo::kInvalidTilingBlockDim)),
-            IfaCase::Param(2, 40, 4096, 128, "BSND", 40, 40, 1.0f, 0, 1, {})));
-
-INSTANTIATE_TEST_SUITE_P(Ifa, Ts_Ifa_Ascend910B2_Case, Tc_Ifa_General_Case);
-INSTANTIATE_TEST_SUITE_P(Ifa, Ts_Ifa_Ascend310P3_Case, Tc_Ifa_General_Case);
-INSTANTIATE_TEST_SUITE_P(Ifa, Ts_Ifa_Ascend910_9591_Case, Tc_Ifa_General_Case);
-
 
 TEST_F(Ts_Ifa_Ascend910B2, case_atten_mask)
 {
