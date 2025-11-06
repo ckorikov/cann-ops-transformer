9c9852a7588b72f27c5fb7944a8f3a7de922a050
xutianze <xutianze2@huawei.com>
2025-11-06 20:15:46 +0800

    修复ut整改时修改错误的头文件
    Co-authored-by: xutianze<xutianze2@huawei.com>
    
    !1967 merge fix_dispatchv2_a5 into master
    
    修复ut整改时修改错误的头文件
    
    Created-by: xutianze
    Commit-by: xutianze
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/ops-transformer-dev!1967

--- mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
+++ mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
@@ -14,11 +14,18 @@
  */
 #include "kernel_operator.h"
 #include "moe_distribute_dispatch_v2_tiling.h"
+#include "moe_distribute_dispatch_v2_full_mesh.h"
+#include "moe_distribute_dispatch_v2.h"
+#if __has_include("../moe_distribute_dispatch/moe_distribute_dispatch_a2.h")
 #include "../moe_distribute_dispatch/moe_distribute_dispatch_a2.h"
 #include "../moe_distribute_dispatch/moe_distribute_dispatch_a2_layered.h"
 #include "../moe_distribute_dispatch/moe_distribute_dispatch_a2_layered_aicpu.h"
-#include "moe_distribute_dispatch_v2_full_mesh.h"
-#include "moe_distribute_dispatch_v2.h"
+#else
+#include "../../moe_distribute_dispatch/op_kernel/moe_distribute_dispatch_a2.h"
+#include "../../moe_distribute_dispatch/op_kernel/moe_distribute_dispatch_a2_layered.h"
+#include "../../moe_distribute_dispatch/op_kernel/moe_distribute_dispatch_a2_layered_aicpu.h"
+#endif
+#endif // defined(__DAV_C310__)
 
 using namespace MoeDistributeDispatchV2Impl;
 using namespace MoeDistributeDispatchA2Impl;
