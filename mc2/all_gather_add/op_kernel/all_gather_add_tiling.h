f29a52d7b11ab141b82a2532d966fe43ca4558c5
lyt_claire <luyitong1@huawei.com>
2025-11-06 20:03:27 +0800

    逻辑修改

--- mc2/all_gather_add/op_kernel/all_gather_add_tiling.h
+++ mc2/all_gather_add/op_kernel/all_gather_add_tiling.h
@@ -21,9 +21,12 @@
 struct AllGatherAddTilingData {
     Mc2InitTiling mc2InitTiling;
     Mc2CcTiling mc2CcTiling;
-    uint32_t totalLength;
-    uint32_t tileNum;
-    uint32_t tileLength;
+    uint32_t commTurn; // 通信轮次，本示例通信1次
+    uint32_t tileNum; // 计算切分轮次
+    uint32_t totalLength; // 总长度是参与Add操作的数据个数
+    uint32_t blockLength; // 每个核处理的数据个数
+    uint32_t tileLength; // 每个分片处理的数据个数
+    uint32_t gatherTileLength; // 待gather的数据个数
 };
 
 #endif //__ALL_GATHER_ADD_TILING_H__
