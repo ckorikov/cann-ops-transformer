51a1aff52c10cf91400722d66b7e2c67db4534be
ChenYiran <chenyiran6@huawei.com>
2025-11-07 16:57:45 +0800

    revert dev仓pr_1887 and tiling struct register

--- mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
+++ mc2/moe_distribute_dispatch_v2/op_kernel/moe_distribute_dispatch_v2.cpp
@@ -14,7 +14,6 @@
  */
 #include "kernel_operator.h"
 #include "moe_distribute_dispatch_v2_tiling.h"
-#include "moe_distribute_dispatch_v2.h"
 
 #if __has_include("../moe_distribute_dispatch/moe_distribute_dispatch_a2.h")
 #include "../moe_distribute_dispatch/moe_distribute_dispatch_a2.h"
@@ -25,6 +24,8 @@
 #include "../../moe_distribute_dispatch/op_kernel/moe_distribute_dispatch_a2_layered.h"
 #include "../../moe_distribute_dispatch/op_kernel/moe_distribute_dispatch_a2_layered_aicpu.h"
 #endif
+
+#include "moe_distribute_dispatch_v2.h"
 
 using namespace MoeDistributeDispatchV2Impl;
 using namespace MoeDistributeDispatchA2Impl;
