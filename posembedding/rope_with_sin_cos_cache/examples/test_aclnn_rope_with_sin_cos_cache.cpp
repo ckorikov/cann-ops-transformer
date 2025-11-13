03af17729ce072baf24b0e18e9988eec71314e2d
fazhenyao123 <fazhenyao@h-partners.com>
2025-11-13 19:37:23 +0800

    rope_with_sin_cos_cache_example
    Co-authored-by: fazhenyao<fazhenyao@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2192 merge rope_with_sin_cos_cache_example into master
    
    rope_with_sin_cos_cache_example
    
    Created-by: fazhenyao123
    Commit-by: fazhenyao
    Merged-by: turing_project1
    Description: ## 描述
    修复rope_with_sin_cos_cache example
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!2192

--- posembedding/rope_with_sin_cos_cache/examples/test_aclnn_rope_with_sin_cos_cache.cpp
+++ posembedding/rope_with_sin_cos_cache/examples/test_aclnn_rope_with_sin_cos_cache.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <vector>
 #include "acl/acl.h"
-#include "aclnnop/level2/aclnn_rope_with_sin_cos_cache.h"
+#include "aclnnop/aclnn_rope_with_sin_cos_cache.h"
 #include <iostream>
 
 #define CHECK_RET(cond, return_expr)                                           \
