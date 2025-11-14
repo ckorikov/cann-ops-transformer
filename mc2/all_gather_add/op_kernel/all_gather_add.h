2851cb70e4e42601c7fb728fba9e7ae5876d5f45
lyt_claire <luyitong1@huawei.com>
2025-11-14 17:27:52 +0800

    hccl Finalize加全核同步

--- mc2/all_gather_add/op_kernel/all_gather_add.h
+++ mc2/all_gather_add/op_kernel/all_gather_add.h
@@ -66,7 +66,7 @@ __aicore__ inline void AllGatherAdd::Init(GM_ADDR aGM, GM_ADDR bGM, GM_ADDR cGM,
     tPipe_ = tPipe;
     blockLength_ = tilingData->blockLength;
     tileLength_ = tilingData->tileLength;
-    // todo 考虑尾块 对齐的话：a :48 2048 b: 96 2048
+    tileNum_ = tilingData->tileNum;
 
     // 初始化hccl对象
     hccl_.InitV2(contextGM, tilingData);
@@ -76,7 +76,7 @@ __aicore__ inline void AllGatherAdd::Init(GM_ADDR aGM, GM_ADDR bGM, GM_ADDR cGM,
     gatherOutGM.SetGlobalBuffer((__gm__ half*)gatherGM + blockLength_ * AscendC::GetBlockIdx() * sizeof(half), blockLength_);
     inputBGM.SetGlobalBuffer((__gm__ half*)bGM + blockLength_ * AscendC::GetBlockIdx() * sizeof(half), blockLength_);
     outputCGM.SetGlobalBuffer((__gm__ half*)cGM + blockLength_ * AscendC::GetBlockIdx() * sizeof(half), blockLength_);
-
+    
     tPipe_->InitBuffer(inputQueueGather, ALLGATHER_ADD_BUFFER_NUM, tileLength_ * sizeof(half));
     tPipe_->InitBuffer(inputQueueB, ALLGATHER_ADD_BUFFER_NUM, tileLength_ * sizeof(half));
     tPipe_->InitBuffer(outputQueueC, ALLGATHER_ADD_BUFFER_NUM, tileLength_ * sizeof(half));
@@ -120,21 +120,20 @@ __aicore__ inline void AllGatherAdd::CopyOut(int32_t progress)
 
 __aicore__ inline void AllGatherAdd::HcclFinalize()
 {
+    AscendC::SyncAll<true>();
     hccl_.Finalize();
 }
 
 __aicore__ inline void AllGatherAdd::Process()
 {
     HcclPrepare();
-    for (int i = 0; i < tilingData_->tileNum; i++) {
+    for (int i = 0; i < tilingData_->commTurn; i++) {
         hccl_.Wait(handleId_);
-        for (int rankId = 0; rankId < hccl_.GetRankDim(); rankId++) {
-            if (rankId == hccl_.GetRankId()) {
-                CopyIn(i);
+         for (int j = 0; j < tileNum_; j++) {
+                CopyIn(j);
                 Compute();
-                CopyOut(i);
-            }
-        }
+                CopyOut(j);
+         }
     }
     HcclFinalize();
 }
