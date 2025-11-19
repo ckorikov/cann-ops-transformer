1b743407c275f3c87a10303f44d46ddcfce478ae
haijie_699874 <zhanghaijie4@hisilicon.com>
2025-11-19 18:10:17 +0800

    打开所有形态 build in包
    Co-authored-by: 张海杰<zhanghaijie4@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2471 merge build_in into master
    
    打开所有形态 build in包
    
    Created-by: haijie_699874
    Commit-by: 张海杰
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!2471

--- build.sh
+++ build.sh
@@ -37,7 +37,8 @@ ENABLE_BUILT_JIT=FALSE
 ENABLE_BUILT_CUSTOM=FALSE
 ENABLE_EXPERIMENTAL=FALSE
 ASCEND_SOC_UNITS="ascend910b"
-SUPPORT_COMPUTE_UNIT_SHORT=("ascend910b" "ascend910_93" "ascend910_95" "ascend310p" "ascend910")
+SUPPORT_COMPUTE_UNIT_SHORT=("ascend031" "ascend035" "ascend310b" "ascend610lite" "ascend910_55" "mc62cm12a"
+                            "ascend910b" "ascend910_93" "ascend910_95" "ascend310p" "ascend910")
 CMAKE_BUILD_MODE=""
 ENABLE_DEBUG=FALSE
 BUILD_LIBS=()
