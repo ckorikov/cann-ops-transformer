b94f41fc7aec940bef07ef3d177772d426f09e1a
dingfeng18 <dingfeng18@huawei.com>
2025-11-26 17:43:45 +0800

    enable 3072 hidden size for mla_prolog
    Co-authored-by: dingfeng<dingfeng18@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2784 merge master into master
    
    enable 3072 hidden size for mla_prolog
    
    Created-by: dingfeng18
    Commit-by: dingfeng
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!2784

--- attention/mla_prolog/op_host/mla_prolog_tiling_check.cpp
+++ attention/mla_prolog/op_host/mla_prolog_tiling_check.cpp
@@ -144,7 +144,7 @@ ge::graphStatus MlaPrologTilingCheck::CheckDims() const
                 ConvertContainerToString(supportedHeSize).c_str(), baseShapeInfo_.heSize),
             return ge::GRAPH_FAILED);
     } else {
-        const std::set<uint32_t> supportedHeSize {7168U, 7680U, 6144U};
+        const std::set<uint32_t> supportedHeSize {1024U, 2048U, 3072U, 4096U, 5120U, 6144U, 7168U, 7680U, 8192U};
         OP_CHECK_IF(supportedHeSize.find(baseShapeInfo_.heSize) == supportedHeSize.end(),
             OP_LOGE(context_.opName, "He allows only %s, got %u.",
                 ConvertContainerToString(supportedHeSize).c_str(), baseShapeInfo_.heSize),
