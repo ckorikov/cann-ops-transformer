c88c54f2a60a1549192324a960ca9c3bc3ea3781
turing_project1 <songchangxia@cann.team>
2025-11-26 14:48:00 +0800

    修正GMM Tiling对mxfp4拦截的错误
    Co-authored-by: liyinghao10<liyinghao10@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2822 merge mxfp4 into master
    
    修正GMM Tiling对mxfp4拦截的错误
    
    Created-by: liyinghao10
    Commit-by: liyinghao10
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!2822

--- gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
+++ gmm/grouped_matmul/op_host/op_tiling/arch35/grouped_quant_matmul_tiling.cpp
@@ -332,7 +332,7 @@ bool GroupedQbmmTiling::CheckFp4Shape() const
                         inputParams_.kSize),
                 return false);
     // 2: mxfp4场景下不支持K轴为2
-    OP_CHECK_IF(inputParams_.kSize != 2,
+    OP_CHECK_IF(inputParams_.kSize == 2,
                 OP_LOGE(inputParams_.opName, "When the dtype of x is FLOAT4, the k size should not be 2"),
                 return false);
     if (!inputParams_.transB) {
