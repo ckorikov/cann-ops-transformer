cc2bfeb87864ca7ce6f34b267abb07b34ce49d6b
xutianze <xutianze2@huawei.com>
2025-12-01 20:21:43 +0800

    ci支持ut和example分开控制开关
    Co-authored-by: xutianze<xutianze2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3105 merge feature_ut_example_ctl into master
    
    ci支持ut和example分开控制开关
    
    Created-by: xutianze
    Commit-by: xutianze
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!3105

--- cmake/scripts/parse_changed_files.py
+++ cmake/scripts/parse_changed_files.py
@@ -29,8 +29,9 @@ class Module:
         self.src_exclude_files: List[Path] = []
         self.tests_ut_ops_test_src_files: List[Path] = []
         self.tests_ut_ops_test_src_exclude_files: List[Path] = []
-        self.tests_ut_ops_test_options: List[str] = []       
+        self.tests_ut_ops_test_options: List[str] = []
         self.options: List[str] = []
+        self.test_excludes: List[str] = []
 
     @staticmethod
     def _add_str_cfg(src, dst: List[str]):
@@ -41,26 +42,33 @@ class Module:
                 dst.append(s)
         return True
 
+    @staticmethod
+    def _add_test_excludes(test_options, dst: List[str]):
+        if isinstance(test_options, Dict):
+            if 'examples' in test_options and not test_options['examples']:
+                dst.append('examples')
+            if 'ut' in test_options and not test_options['ut']:
+                dst.append('ut')
+        return True
+
     def update_classify_cfg(self, desc: Dict[str, Any]) -> bool:
         if not self._update_src(desc=desc):
             return False
         if not self._update_exclude_src(desc=desc):
             return False
-        if not self._update_tests(desc=desc):
-            return False
-        if not self._check():
+        if not self._update_test_excludes(desc=desc):
             return False
         if not self._update_options(desc=desc):
             return False
         return True
 
-    def get_test_ut_ops_test_options(self, f: Path) -> List[str]:
+    def get_test_options(self, f: Path) -> List[str]:
 
         def is_excluded(e_f: Path):
             for e in self.src_exclude_files:
                 try:
-                    if e_f.relative_to(e):
-                        return True
+                    e_f.relative_to(e)
+                    return True
                 except ValueError:
                     continue
             return False
@@ -80,7 +88,7 @@ class Module:
         return related_options
 
     def get_test_example_ops_test_options(self, f: Path) -> List[str]:
-        return self.get_test_ut_ops_test_options(f)
+        return self.get_test_options(f)
 
     def print_details(self):
         dbg_str = (f"Name={self.name} SrcLen={len(self.src_files)} "
@@ -109,37 +117,13 @@ class Module:
         src_paths = desc.get('exclude', [])
         return self._add_rel_path(src=src_paths, dst=self.src_exclude_files)
 
-    def _update_tests(self, desc: Dict[str, Any]) -> bool:
-        tests_desc = desc.get('tests', {})
-        for sub_name, sub_desc in tests_desc.items():
-            if sub_name == 'ut':
-                if not self._update_ut(desc=sub_desc):
-                    return False
-        return True
-    
+    def _update_test_excludes(self, desc: Dict[str, Any]) -> bool:
+        test_options = desc.get('test', [])
+        return self._add_test_excludes(test_options=test_options, dst=self.test_excludes)
+
     def _update_options(self, desc: Dict[str, Any]) -> bool:
         options = desc.get('options', [])
         return self._add_str_cfg(src=options, dst=self.options)
-
-    def _update_ut(self, desc: Dict[str, Any]) -> bool:
-        for sub_name, sub_desc in desc.items():
-            if sub_name == "ops_test":
-                src_files = sub_desc.get('src', [])
-                if not self._add_rel_path(src=src_files, dst=self.tests_ut_ops_test_src_files):
-                    return False
-                src_exclude_files = sub_desc.get('exclude', [])
-                if not self._add_rel_path(src=src_exclude_files, dst=self.tests_ut_ops_test_src_exclude_files):
-                    return False
-                options = sub_desc.get('options', [])
-                if not self._add_str_cfg(src=options, dst=self.tests_ut_ops_test_options):
-                    return False
-        return True
-
-    def _check(self) -> bool:
-        if len(self.src_files) != 0 or len(self.tests_ut_ops_test_src_files) != 0:
-            return True
-        logging.error('[%s] don\'t set any sources.', self.name)
-        return False
 
 
 class Parser:
@@ -149,6 +133,8 @@ class Parser:
 
     _Modules: List[Module] = []         # 保存规则文件(classify_rule)内设置的模块列表
     _ChangedPaths: List[Path] = []      # 修改文件列表文件(changed_file)内设置的修改文件列表
+    _UTExcludes: List[str] = []
+    _ExamplesExcludes: List[str] = []
 
     @classmethod
     def print_details(cls):
@@ -165,10 +151,23 @@ class Parser:
             return False
         with open(file, 'r', encoding='utf-8') as f:
             desc: Dict[str, Any] = yaml.load(f, Loader=yaml.SafeLoader)
-        for name, sub_desc in desc.items():
-            if not cls._parse_classify_item(name=name, desc=sub_desc):
-                return False
-        return True
+
+        def extract_from_dict(obj, current_key='root') -> bool:
+            # 只看 dict 类型
+            if not isinstance(obj, dict):
+                return True
+
+            # 递归到 module 时说明到达最后一层
+            if 'module' in obj:
+                return cls._parse_classify_item(current_key, desc)
+
+            # 递归处理其他值
+            for key, value in obj.items():
+                if not extract_from_dict(value, key):
+                    return False
+            return True
+
+        return extract_from_dict(desc)
 
     @classmethod
     def parse_changed_file(cls, file: Path) -> bool:
@@ -192,26 +191,23 @@ class Parser:
         ops_test_option_lst: List[str] = []
         for p in cls._ChangedPaths:
             for m in cls._Modules:
-                new_options = m.get_test_ut_ops_test_options(f=p)
+                new_options = m.get_test_options(f=p)
                 for opt in new_options:
                     if opt not in ops_test_option_lst:
                         ops_test_option_lst.append(opt)
-                        logging.info("TESTS_UT_OPS_TEST [%s] is trigger!", opt)
+                        logging.info("OPs [%s] UT is trigger!", opt)
         if len(ops_test_option_lst) == 0:
-            logging.info("Don't trigger any target.")
+            logging.info("Don't trigger any UT.")
             return ""
         ops_test_ut_str: str = ""
         if "all" in ops_test_option_lst:
             ops_test_ut_str = "all"
         else:
             for opt in ops_test_option_lst:
-                ops_test_ut_str += f"{opt};"
+                if opt not in cls._UTExcludes:
+                    ops_test_ut_str += f"{opt};"
         ops_test_ut_str = f"{ops_test_ut_str}"
         return ops_test_ut_str
-
-    @classmethod
-    def get_related_st(cls) -> str:
-        return ""
 
     @classmethod
     def get_ops_test_option_lst(cls) -> List[str]:
@@ -222,21 +218,22 @@ class Parser:
                 for opt in new_options:
                     if opt not in ops_test_option_lst:
                         ops_test_option_lst.append(opt)
-                        logging.info("TESTS_RUN_EXAMPLE_OPS_TEST [%s] is trigger!", opt)
+                        logging.info("OPs [%s] examples is trigger!", opt)
         return ops_test_option_lst
 
     @classmethod
     def get_related_examples(cls) -> str:
         ops_test_option_lst = cls.get_ops_test_option_lst()
         if len(ops_test_option_lst) == 0:
-            logging.info("Don't trigger any target.")
+            logging.info("Don't trigger any examples.")
             return ""
         ops_test_examples_str: str = ""
         if "all" in ops_test_option_lst:
             ops_test_examples_str = "all"
         else:
             for opt in ops_test_option_lst:
-                ops_test_examples_str += f"{opt};"
+                if opt not in cls._ExamplesExcludes:
+                    ops_test_examples_str += f"{opt};"
         ops_test_examples_str = f"{ops_test_examples_str}"
         return ops_test_examples_str
 
@@ -250,6 +247,11 @@ class Parser:
             rst = mod.update_classify_cfg(desc=desc)
             if rst:
                 cls._Modules.append(mod)
+                short_name = name.split('/')[-1]
+                if 'examples' in mod.test_excludes:
+                    cls._ExamplesExcludes.append(short_name)
+                if 'ut' in mod.test_excludes:
+                    cls._UTExcludes.append(short_name)
             return rst
         for k, sub_desc in desc.items():
             if not cls._parse_classify_item(name=name + '/' + k, desc=sub_desc):
@@ -266,8 +268,6 @@ class Parser:
         sub_ps = ps.add_subparsers(help="Sub-Command")
         p_ut = sub_ps.add_parser('get_related_ut', help="Get related ut.")
         p_ut.set_defaults(func=Parser.get_related_ut)
-        p_st = sub_ps.add_parser('get_related_st', help="Get related st.")
-        p_st.set_defaults(func=Parser.get_related_st)
         p_examples = sub_ps.add_parser('get_related_examples', help="Get related examples.")
         p_examples.set_defaults(func=Parser.get_related_examples)
         # 处理
@@ -283,5 +283,5 @@ class Parser:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(filename)s:%(lineno)d [%(levelname)s] %(message)s', level=logging.INFO)
+    logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
     print(Parser.main())
