809b164a6f938db76f30836b6650cf855a13ccc3
wujunliang <wujunliang2@huawei.com>
2025-12-02 17:01:08 +0800

    【bugfix】MatmulAllReduce修复mxfp4 精度问题
    Co-authored-by: 吴君亮<wujunliang2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2628 merge dts into master
    
    【bugfix】MatmulAllReduce修复mxfp4 精度问题
    
    Created-by: wujunliang
    Commit-by: 吴君亮
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer-dev!2628

--- mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_base.h
+++ mc2/matmul_all_reduce/op_kernel/arch35/matmul_all_reduce_base.h
@@ -59,12 +59,22 @@ public:
         const uint64_t mVal = isOneTileFlag_ ? ((uint64_t)paramInTiling_->rankM) : (uint64_t)tileInfo_.mmTiling->M;
 
         tileInfo_.aOffset = mVal * (uint64_t)tileInfo_.mmTiling->Ka;
-        tileInfo_.aAddrOffset = tileInfo_.aOffset * sizeof(XType);
+        if (AscendC::IsSameType<XType, fp4x2_e2m1_t>::value || AscendC::IsSameType<XType, fp4x2_e1m2_t>::value) {
+            // In 4-bits scenario, the data length needs to be divided by 2.
+            tileInfo_.aAddrOffset = tileInfo_.aOffset * sizeof(XType) / 2;
+        } else {
+            tileInfo_.aAddrOffset = tileInfo_.aOffset * sizeof(XType);
+        }
         tileInfo_.cOffset = mVal * (uint64_t)tileInfo_.mmTiling->N;
         tileInfo_.cAddrOffset = tileInfo_.cOffset * sizeof(YType);
         if (tailFlag_) {
             tailInfo_.aOffset = (uint64_t)tailInfo_.mmTiling->M * (uint64_t)tailInfo_.mmTiling->Ka;
-            tailInfo_.aAddrOffset = tailInfo_.aOffset * sizeof(XType);
+            if (AscendC::IsSameType<XType, fp4x2_e2m1_t>::value || AscendC::IsSameType<XType, fp4x2_e1m2_t>::value) {
+                // In 4-bits scenario, the data length needs to be divided by 2.
+                tileInfo_.aAddrOffset = tileInfo_.aOffset * sizeof(XType) / 2;
+            } else {
+                tailInfo_.aAddrOffset = tailInfo_.aOffset * sizeof(XType);
+            }
             tailInfo_.cOffset = (uint64_t)tailInfo_.mmTiling->M * (uint64_t)tailInfo_.mmTiling->N;
             tailInfo_.cAddrOffset = tailInfo_.cOffset * sizeof(YType);
         }
