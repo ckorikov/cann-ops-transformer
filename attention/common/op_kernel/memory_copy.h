042b238bd0f5651e07109c1a23a3d3af03f006a8
l00935273 <liyangzhi1@huawei.com>
2025-12-06 18:34:50 +0800

    modify consistent with dev

--- attention/common/op_kernel/memory_copy.h
+++ attention/common/op_kernel/memory_copy.h
@@ -1490,10 +1490,6 @@ private:
         // N2*G*T(BS1)*D
         OffsetCalculator<GM_FORMAT> &offsetCalculator = srcTensor.offsetCalculator;
         uint64_t s1Size = 0;
-        uint32_t gIdxStart = 0;
-        uint32_t s1IdxStart = 0;
-        uint32_t gIdxEnd = 0;
-        uint32_t s1IdxEnd = 0;
 
         if constexpr (GmLayoutParams<GM_FORMAT>::CATEGORY == FormatCategory::GM_Q_OUT_TND) {
             s1Size = offsetCalculator.actualSeqLensQParser.GetActualSeqLength(gmCoord.bIdx);
@@ -1505,12 +1501,10 @@ private:
             }
         }
         
-        if (s1Size != 0) {
-            gIdxStart = gmCoord.gS1Idx / s1Size;
-            s1IdxStart = gmCoord.gS1Idx % s1Size;
-            gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
-            s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;
-        }
+        uint32_t gIdxStart = gmCoord.gS1Idx / s1Size;
+        uint32_t s1IdxStart = gmCoord.gS1Idx % s1Size;
+        uint32_t gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
+        uint32_t s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;
 
         uint64_t queryGmbaseOffset =
             offsetCalculator.GetOffset(gmCoord.bIdx, gmCoord.n2Idx, gIdxStart, 0, gmCoord.dIdx);
@@ -1596,10 +1590,6 @@ public:
         if constexpr (UB_FORMAT == UbFormat::GS1) {
             OffsetCalculator<GM_FORMAT> &offsetCalculator = dstTensor.offsetCalculator;
             uint32_t s1Size = 0;
-            uint32_t gIdxStart = 0;
-            uint32_t s1IdxStart = 0;
-            uint32_t gIdxEnd = 0;
-            uint32_t s1IdxEnd = 0;
 
             if constexpr (GmLayoutParams<GM_FORMAT>::CATEGORY == FormatCategory::GM_Q_OUT_TND) {
                 s1Size = offsetCalculator.actualSeqLensQParser.GetActualSeqLength(gmCoord.bIdx);
@@ -1610,12 +1600,10 @@ public:
                     s1Size = offsetCalculator.GetDimS1();
                 }
             }
-            if (s1Size != 0) {
-                gIdxStart = gmCoord.gS1Idx / s1Size;
-                s1IdxStart = gmCoord.gS1Idx % s1Size;
-                gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
-                s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;
-            }
+            uint32_t gIdxStart = gmCoord.gS1Idx / s1Size;
+            uint32_t s1IdxStart = gmCoord.gS1Idx % s1Size;
+            uint32_t gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
+            uint32_t s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;
 
             uint64_t attenOutGmbaseOffset = offsetCalculator.GetOffset(gmCoord.bIdx, gmCoord.n2Idx, gIdxStart, 0, 0);
 
@@ -2034,10 +2022,6 @@ private:
     {
         OffsetCalculator<GM_FORMAT> &offsetCalculator = srcTensor.offsetCalculator;
         uint64_t s1Size = 0;
-        uint32_t gIdxStart = 0;
-        uint32_t s1IdxStart = 0;
-        uint32_t gIdxEnd = 0;
-        uint32_t s1IdxEnd = 0;
 
         if constexpr (GmLayoutParams<GM_FORMAT>::CATEGORY == FormatCategory::GM_Q_OUT_TND) {
             s1Size = offsetCalculator.actualSeqLensQParser.GetActualSeqLength(gmCoord.bIdx);
@@ -2048,12 +2032,11 @@ private:
                 s1Size = offsetCalculator.GetDimS1();
             }
         }
-        if (s1Size != 0) {
-            gIdxStart = gmCoord.gS1Idx / s1Size;
-            s1IdxStart = gmCoord.gS1Idx % s1Size;
-            gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
-            s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;
-        }
+        
+        uint32_t gIdxStart = gmCoord.gS1Idx / s1Size;
+        uint32_t s1IdxStart = gmCoord.gS1Idx % s1Size;
+        uint32_t gIdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) / s1Size;
+        uint32_t s1IdxEnd = (gmCoord.gS1Idx + gmCoord.gS1DealSize) % s1Size;    
         uint64_t queryGmbaseOffset = offsetCalculator.GetOffset(gmCoord.bIdx, gmCoord.n2Idx, gIdxStart, 0, gmCoord.dIdx);
 
         // 处理 首行
