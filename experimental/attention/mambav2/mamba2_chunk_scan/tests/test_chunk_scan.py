446c1ed7fcc1c15528230d6b0ff6397b29693edb
Jing_Huang66 <huangjing66@huawei.com>
2025-12-09 18:11:09 +0800

    update_mambav2

--- experimental/attention/mambav2/mamba2_chunk_scan/tests/test_chunk_scan.py
+++ experimental/attention/mambav2/mamba2_chunk_scan/tests/test_chunk_scan.py
@@ -16,7 +16,7 @@ print = Console().print
 print(Text('VALIDATING OUTPUT...', style=Style(color='red', underline=True)))
 
 import npu_ops_transformer_ext
-
+from utils.utils import check_diff, profiling
 
 def mamba2_chunkscan_custom_fwd(C, B, input_dacs, input_dttrans, input_mask, x, input_outa, input_dmtx, chunk_size=256, ngroups=8, headdim=64, z=None, seq_idx=None):
     #preprocess
@@ -32,7 +32,6 @@ def mamba2_chunkscan_custom_fwd(C, B, input_dacs, input_dttrans, input_mask, x, 
     padLen = nchunks * chunk_size - seqlen 
     B = F.pad(B, (0,0,0,0,0,padLen), "constant", 0)
     B = B.reshape(batch, nchunks, chunk_size, ngroups, dstate)
-    # B = torch.repeat_interleave(B, nheads//ngroups, dim=3)
     B = torch.permute(B, (0, 1, 3, 4, 2))  
 
     batch, seqlen, nheads, headdim = x.shape
@@ -42,14 +41,11 @@ def mamba2_chunkscan_custom_fwd(C, B, input_dacs, input_dttrans, input_mask, x, 
 
     C = F.pad(C, (0,0,0,0,0,padLen), "constant", 0)
     C = C.reshape(batch, nchunks, chunk_size, ngroups, dstate)
-    # C = torch.repeat_interleave(C, nheads//ngroups, dim=3)
     C = C.permute(0, 1, 3, 2, 4)  
     batch, nchunks, chunk_size, nheads, headdim = x.shape
 
-    # c.dtype=half, b.dtype=half, accum.dtype=fp32, out.dtype=fp32
-    cb = torch.matmul(C.to(torch.float32), B.to(torch.float32)).to(torch.float32) #bmtx和cmtx写反了 应该是cb，不是bc
+    cb = torch.matmul(C.to(torch.float32), B.to(torch.float32)).to(torch.float32) 
     out_cb = cb
-    # print(input_dacs.shape)
     cb = torch.repeat_interleave(cb, nheads//ngroups, dim=2)
     dummy = (input_dacs.reshape([batch, nchunks, nheads, chunk_size,1]) - input_dacs.reshape([batch, nchunks, nheads,1, chunk_size]))
     
@@ -70,7 +66,7 @@ def mamba2_chunkscan_custom_fwd(C, B, input_dacs, input_dttrans, input_mask, x, 
     x_res = x.to(torch.float32) * input_dmtx[..., None].to(torch.float32)
     out += x_res.permute(0,1,3,2,4)
     out = out.transpose(2,3)
-    y = out.reshape(batch, -1, nheads, headdim)
+    y = out.reshape(batch, -1, nheads*headdim)
 
     return out_cb, out_m, out_b, y
 
@@ -85,25 +81,15 @@ if __name__ == '__main__':
     N = 128
     P = 64
 
-    device = torch.device("npu:6")
-    
-    cmtx = np.random.random([(B * C * L * G * N)]).astype(np.float16) * 0.5
-    bmtx = np.random.random([(B * C * L * G * N)]).astype(np.float16) * 0.5
-    xmtx = np.random.random([(B * C * L * H * P)]).astype(np.float16) * 0.5
-    dmtx = np.random.random([(H)]).astype(np.float16) * 0.5
-    input_dacs = np.random.random([(B * C * H * L)]).astype(np.float32) * 0.5
-    input_dtout = np.random.random([(B * C * H * L)]).astype(np.float32) * 0.5
-    input_outa = np.random.random([(B * C * H * L * P)]).astype(np.float32) * 0.5
-    dmtx = np.random.random([H]).astype(np.float16) * 0.5
-    
-    
-    tensor_cmtx = torch.from_numpy(cmtx).reshape(B,C*L,G*N).to(device)
-    tensor_bmtx = torch.from_numpy(bmtx).reshape(B,C*L,G*N).to(device)
-    tensor_xmtx = torch.from_numpy(xmtx).reshape(B,C*L,H*P).to(device)
-    tensor_dacs = torch.from_numpy(input_dacs).reshape(B,C,H,L).to(device)
-    tensor_dtout = torch.from_numpy(input_dtout).reshape(B,C,H,L).to(device)
-    tensor_outa = torch.from_numpy(input_outa).reshape(B,C,H,L,P).to(device)
-    tensor_dmtx = torch.from_numpy(dmtx).to(device)
+    device = torch.device("npu:0")
+
+    tensor_cmtx = torch.randn([B, C*L, G*N], dtype=torch.float16, device=device) * 0.2
+    tensor_bmtx = torch.randn([B, C*L, G*N], dtype=torch.float16, device=device) * 0.2
+    tensor_xmtx = torch.randn([B, C*L, H*P], dtype=torch.float16, device=device) * 0.2
+    tensor_dacs = torch.randn([B, C, H, L], dtype=torch.float32, device=device) * 0.2
+    tensor_dtout = torch.randn([B, C, H, L], dtype=torch.float32, device=device) * 0.2
+    tensor_outa = torch.randn([B, C, H, L, P], dtype=torch.float32, device=device) * 0.2
+    tensor_dmtx = torch.randn([H], dtype=torch.float16, device=device) * 0.2
     mask = torch.triu(torch.ones(L, L), diagonal=1).to(device)
     
     out_cb, out_m, out_b, out_y = mamba2_chunkscan_custom_fwd(tensor_cmtx, tensor_bmtx, tensor_dacs, tensor_dtout, mask, tensor_xmtx, tensor_outa, tensor_dmtx, chunk_size=L)
@@ -119,137 +105,15 @@ if __name__ == '__main__':
             )
     check_diff(out_y.cpu().numpy(), npu_y.cpu().numpy())
 
-
-    if 0:
-        for i in range(10):
-            out_cb, out_m, out_b, out_y = mamba2_chunkscan_custom_fwd(tensor_cmtx, tensor_bmtx, tensor_dacs, tensor_dtout, mask, tensor_xmtx, tensor_outa, tensor_dmtx, chunk_size=L)
-            
-        torch.npu.synchronize()
-        start = time.time()
-        for i in range(1000):
-            out_cb, out_m, out_b, out_y = mamba2_chunkscan_custom_fwd(tensor_cmtx, tensor_bmtx, tensor_dacs, tensor_dtout, mask, tensor_xmtx, tensor_outa, tensor_dmtx, chunk_size=L)
-        torch.npu.synchronize()
-        end = time.time()
-        print(f'torch time: {(end - start)} ms')
-        
-        
-        npu_cb = torch.zeros_like(out_cb)
-        npu_m = torch.zeros_like(out_m)
-        npu_b = torch.zeros_like(out_b)
-        npu_y = torch.zeros_like(out_y)
-        npu_workspace = torch.empty([20 * L * L]).to(torch.float32).npu()
-        
-        print(f'{tensor_cmtx.reshape(B,C,L,G,N).shape}, {tensor_cmtx.dtype}')
-        print(f'{tensor_bmtx.shape}, {tensor_bmtx.dtype}')
-        print(f'{tensor_xmtx.shape}, {tensor_xmtx.dtype}')
-        print(f'{tensor_dmtx.shape}, {tensor_dmtx.dtype}')
-        print(f'{tensor_outa.shape}, {tensor_outa.dtype}')
-        print(f'{tensor_dacs.shape}, {tensor_dacs.dtype}')
-        print(f'{tensor_dtout.shape}, {tensor_dtout.dtype}')
-        print(f'{npu_cb.shape}, {npu_cb.dtype}')
-        print(f'{npu_m.shape}, {npu_m.dtype}')
-        print(f'{npu_b.shape}, {npu_b.dtype}')
-        print(f'{npu_y.shape}, {npu_y.dtype}')
-        print(f'{npu_workspace.shape} {npu_workspace.dtype}')
-        
-        for i in range(5):
-            kernel_cust_mamba2_chunk_scan.kernel_cust_mamba2_chunk_scan(
-                tensor_cmtx.reshape(B,C,L,G,N),
+    profiling(mamba2_chunkscan_custom_fwd,
+              [tensor_cmtx, tensor_bmtx, tensor_dacs, tensor_dtout, mask, tensor_xmtx, tensor_outa, tensor_dmtx, L],
+              'TORCH')
+    profiling(torch.ops.npu_ops_transformer_ext.mambav2_chunk_scan,
+              [tensor_cmtx.reshape(B,C,L,G,N),
                 tensor_bmtx,
                 tensor_xmtx,
                 tensor_dmtx,
                 tensor_outa,
                 tensor_dacs,
-                tensor_dtout,
-                npu_cb,
-                npu_m,
-                npu_b,
-                npu_y,
-                npu_workspace
-            )
-        
-        ######## PROFILING ########
-        repeat = 10
-        experimental_config = torch_npu.profiler._ExperimentalConfig(profiler_level=torch_npu.profiler.ProfilerLevel.Level2)
-        with torch_npu.profiler.profile(
-            activities=[
-                    torch_npu.profiler.ProfilerActivity.CPU,
-                    torch_npu.profiler.ProfilerActivity.NPU
-                ],
-            schedule=torch_npu.profiler.schedule(wait=0, warmup=0, active=1, repeat=1, skip_first=0),
-            experimental_config = experimental_config,
-            on_trace_ready=torch_npu.profiler.tensorboard_trace_handler('./npu_results_B%d_S%d_H%d'%(B,C*L,H))
-        ) as prof:
-
-            kernel_cust_mamba2_chunk_scan.kernel_cust_mamba2_chunk_scan(
-                tensor_cmtx.reshape(B,C,L,G,N),
-                tensor_bmtx,
-                tensor_xmtx,
-                tensor_dmtx,
-                tensor_outa,
-                tensor_dacs,
-                tensor_dtout,
-                npu_cb,
-                npu_m,
-                npu_b,
-                npu_y,
-                npu_workspace
-            )
-
-            torch.npu.synchronize()
-            t1 = torch.npu.Event(enable_timing=True)
-            t2 = torch.npu.Event(enable_timing=True)
-            t1.record()
-            for _ in range(repeat):
-                kernel_cust_mamba2_chunk_scan.kernel_cust_mamba2_chunk_scan(
-                    tensor_cmtx.reshape(B,C,L,G,N),
-                    tensor_bmtx,
-                    tensor_xmtx,
-                    tensor_dmtx,
-                    tensor_outa,
-                    tensor_dacs,
-                    tensor_dtout,
-                    npu_cb,
-                    npu_m,
-                    npu_b,
-                    npu_y,
-                    npu_workspace
-                )
-            t2.record()
-            torch.npu.synchronize()
-            elapsed = t1.elapsed_time(t2)
-
-            print('NPU KERNEL kernel_cust_mamba2_chunk_scan TIME ELAPSED: %.1f'%(elapsed/repeat*1000), 'us')
-
-            prof.step()
-        ######## END OF PROFILING ########
-        torch.npu.synchronize()
-        start = time.time()
-        for i in range(1000):
-            kernel_cust_mamba2_chunk_scan.kernel_cust_mamba2_chunk_scan(
-                tensor_cmtx.reshape(B,C,L,G,N),
-                tensor_bmtx,
-                tensor_xmtx,
-                tensor_dmtx,
-                tensor_outa,
-                tensor_dacs,
-                tensor_dtout,
-                npu_cb,
-                npu_m,
-                npu_b,
-                npu_y,
-                npu_workspace
-            )
-        torch.npu.synchronize()
-        end = time.time()
-        print(f'kernel time: {(end - start)} ms')
-        
-        # check_diff(gt_cbmtx, npu_cb.cpu().numpy().reshape(-1), 'cube0')
-        # check_diff(gt_mmtx, npu_m.cpu().numpy().reshape(-1), 'vec0')
-        # check_diff(gt_outmtx, npu_b.cpu().numpy().reshape(-1), 'cube1')
-        # check_diff(gt_out_y, npu_y.cpu().numpy().reshape(-1), 'vec1')
-    
-        check_diff(out_cb.cpu().numpy(), npu_cb.cpu().numpy(), 'cube0')
-        check_diff(out_m.cpu().numpy(), npu_m.cpu().numpy(), 'vec0')
-        check_diff(out_b.cpu().numpy(), npu_b.cpu().numpy(), 'cube1')
-        check_diff(out_y.cpu().numpy(), npu_y.cpu().numpy(), 'vec1')
+                tensor_dtout],
+              'NPU_KERNEL')
--- experimental/attention/mambav2/mamba2_chunk_state_passing/tests/test_chunk_state_passing.py
+++ experimental/attention/mambav2/mamba2_chunk_state_passing/tests/test_chunk_state_passing.py
@@ -14,18 +14,7 @@ print(Text('VALIDATING OUTPUT...', style=Style(color='red', underline=True)))
 
 # npu kernel
 import npu_ops_transformer_ext
-
-def check_diff(x, y):
-    diff = np.abs(x - y)
-    arg = np.argmax(diff.flatten())
-    print('Left:', x.flatten()[arg], 'Right:', y.flatten()[arg], 'Index:', arg, 'Pass: ', diff.max()<0.005)
-
-
-def unpack_s4tos8(x):
-    x1 = (((x & 0xf) << 4) >> 4)
-    x2 = (x & np.uint8(0xf0).view(np.int8)) >> 4
-    x_unpack = np.stack([x1, x2], axis=-1).flatten()
-    return x_unpack
+from utils.utils import check_diff, profiling
 
 def chunk_state_passing_fwd(dacs, initial_states, states, cmtx, P=64, out_dtype=None):
     batch, nchunks, nheads, Z = states.shape
@@ -36,7 +25,7 @@ def chunk_state_passing_fwd(dacs, initial_states, states, cmtx, P=64, out_dtype=
 
     out_dtype = states.dtype if out_dtype is None else out_dtype
     out = torch.zeros((batch, nchunks, nheads, Z), device=states.device, dtype=out_dtype)
-    final_states = torch.zeros((batch, nheads, Z), device=states.device, dtype=torch.float32)
+    final_state = torch.zeros((batch, nheads, Z), device=states.device, dtype=torch.float32)
 
     tmp_states = torch.zeros((batch, nheads, Z), device=states.device, dtype=out_dtype)
 
@@ -58,7 +47,7 @@ def chunk_state_passing_fwd(dacs, initial_states, states, cmtx, P=64, out_dtype=
     out = out.reshape(batch, nchunks, nheads, N, P).to(cmtx.dtype)
     final_state = final_state.reshape(batch, nheads, N, P)
     
-    out_a = torch.matmul(cmtx.to(torch.float32), out.to(torch.float32))  # B, 4, 80, 256, 64  # original mm using fp16
+    out_a = torch.matmul(cmtx.to(torch.float32), out.to(torch.float32))  # B, 4, 80, 256, 64  
 
     return out, final_state, out_a
 
@@ -75,128 +64,24 @@ if __name__ == '__main__':
     Z = (N * P)
     
     device = torch.device("npu:6")
+    print("torch compute")
 
-    dacs = np.random.random([((B * C) * H)]).astype(np.float32) * 0.5
-    initmtx = np.random.random([((B * H) * Z)]).astype(np.float32) * 0.5
-    statemtx = np.random.random([(((B * C) * H) * Z)]).astype(np.float32) * 0.5
-    cmtx = np.random.random([(((B * S) * G) * N)]).astype(np.float16) * 0.5
-    out_states = np.random.random([((((B * C) * H) * N) * P)]).astype(np.float32) * 0.5
-    final_state = np.random.random([(((B * H) * N) * P)]).astype(np.float32) * 0.5
-    out_bmm = np.random.random([((((B * C) * H) * L) * P)]).astype(np.float32) * 0.5
-
-    ##### torch function golden ########
-    tensor_dacs = torch.from_numpy(dacs).reshape(B,C,H).to(device)
-    tensor_initmtx = torch.from_numpy(initmtx).reshape(B,H,Z).to(device)
-    tensor_statemtx = torch.from_numpy(statemtx).reshape(B,C,H,Z).to(device)
-    tensor_cmtx = torch.from_numpy(cmtx).reshape(B,C,L,G,N).to(device)
+    tensor_dacs = torch.randn([B, C, H], dtype=torch.float32, device=device) * 0.2
+    tensor_initmtx = torch.randn([B, H, Z], dtype=torch.float32, device=device) * 0.2
+    tensor_statemtx = torch.randn([B, C, H, Z], dtype=torch.float32, device=device) * 0.2
+    tensor_cmtx = torch.randn([B, C, L, G, N], dtype=torch.float16, device=device) * 0.2
 
     out_states, final_state, out_bmm = chunk_state_passing_fwd(tensor_dacs, tensor_initmtx, tensor_statemtx, tensor_cmtx)
-    print(tensor_dacs.is_contiguous(), tensor_initmtx.is_contiguous(), tensor_statemtx.is_contiguous(), tensor_cmtx.is_contiguous())
     npu_out, npu_final_state = torch.ops.npu_ops_transformer_ext.mambav2_chunk_state_passing(tensor_dacs, tensor_initmtx, tensor_statemtx, tensor_cmtx)
 
-    check_diff(final_state.cpu().numpy(), npu_final_state.cpu().numpy())
-    check_diff(out_bmm.cpu().numpy(), npu_out.cpu().numpy())
+    check_diff(final_state.cpu(), npu_final_state.cpu())
+    check_diff(out_bmm.cpu(), npu_out.cpu())
 
-    if 0:
-        for i in range(5):
-            out_states, final_state, out_bmm = chunk_state_passing_fwd(tensor_dacs, tensor_initmtx, tensor_statemtx, tensor_cmtx)
-
-        torch.npu.synchronize()
-        start = time.time()
-        for i in range(1000):
-            out_states, final_state, out_bmm = chunk_state_passing_fwd(tensor_dacs, tensor_initmtx, tensor_statemtx, tensor_cmtx)
-        torch.npu.synchronize()
-        end = time.time()
-        print("torch time: ", (end - start), 'ms')
-
-        # npu kernel extension
-        npu_out = torch.zeros_like(out_bmm)
-        npu_final_state = torch.zeros_like(final_state)
-        npu_workspace = torch.zeros([B*H*Z + 20*Z*3]).to(torch.float32).npu()
-        
-        print(f'{tensor_dacs.shape} {tensor_dacs.dtype}')
-        print(f'{tensor_initmtx.shape} {tensor_initmtx.dtype}')
-        print(f'{tensor_statemtx.shape} {tensor_statemtx.dtype}')
-        print(f'{tensor_cmtx.shape} {tensor_cmtx.dtype}')
-        print(f'{npu_out.shape} {npu_out.dtype}')
-        print(f'{npu_final_state.shape} {npu_final_state.dtype}')
-        print(f'{npu_workspace.shape} {npu_workspace.dtype}')
-        
-        
-        for i in range(5):
-            kernel_cust_mamba2_chunk_state_passing.kernel_cust_mamba2_chunk_state_passing(
-                tensor_dacs,
-                tensor_initmtx,
-                tensor_statemtx,
-                tensor_cmtx,
-                npu_out,
-                npu_final_state,
-                npu_workspace
-            )
-
-        ######## PROFILING ########
-        repeat = 10
-        experimental_config = torch_npu.profiler._ExperimentalConfig(profiler_level=torch_npu.profiler.ProfilerLevel.Level2)
-        with torch_npu.profiler.profile(
-            activities=[
-                    torch_npu.profiler.ProfilerActivity.CPU,
-                    torch_npu.profiler.ProfilerActivity.NPU
-                ],
-            schedule=torch_npu.profiler.schedule(wait=0, warmup=0, active=1, repeat=1, skip_first=0),
-            experimental_config = experimental_config,
-            on_trace_ready=torch_npu.profiler.tensorboard_trace_handler('./npu_results_B%d_S%d_H%d'%(B,C*L,H))
-        ) as prof:
-
-            kernel_cust_mamba2_chunk_state_passing.kernel_cust_mamba2_chunk_state_passing(
-                tensor_dacs,
-                tensor_initmtx,
-                tensor_statemtx,
-                tensor_cmtx,
-                npu_out,
-                npu_final_state,
-                npu_workspace
-            )
-
-            torch.npu.synchronize()
-            t1 = torch.npu.Event(enable_timing=True)
-            t2 = torch.npu.Event(enable_timing=True)
-            t1.record()
-            for _ in range(repeat):
-                kernel_cust_mamba2_chunk_state_passing.kernel_cust_mamba2_chunk_state_passing(
-                tensor_dacs,
-                tensor_initmtx,
-                tensor_statemtx,
-                tensor_cmtx,
-                npu_out,
-                npu_final_state,
-                npu_workspace
-            )
-            t2.record()
-            torch.npu.synchronize()
-            elapsed = t1.elapsed_time(t2)
-
-            print('NPU KERNEL kernel_cust_mamba2_chunk_state_passing TIME ELAPSED: %.1f'%(elapsed/repeat*1000), 'us')
-
-            prof.step()
-        ######## END OF PROFILING ########
-            
-        torch.npu.synchronize()
-        start = time.time()
-        for i in range(1000):
-            kernel_cust_mamba2_chunk_state_passing.kernel_cust_mamba2_chunk_state_passing(
-                tensor_dacs,
-                tensor_initmtx,
-                tensor_statemtx,
-                tensor_cmtx,
-                npu_out,
-                npu_final_state,
-                npu_workspace
-            )
-        torch.npu.synchronize()
-        end = time.time()
-        print("kernel time: ", (end - start), 'ms')
-
-
-        check_diff(final_state.cpu().numpy(), npu_final_state.cpu().numpy())
-        check_diff(out_bmm.cpu().numpy(), npu_out.cpu().numpy())
+    inputs = [tensor_dacs, tensor_initmtx, tensor_statemtx, tensor_cmtx]
+    profiling(chunk_state_passing_fwd,
+              inputs,
+              'TORCH')
+    profiling(torch.ops.npu_ops_transformer_ext.mambav2_chunk_state_passing,
+              inputs,
+              'NPU_KERNEL')
     
