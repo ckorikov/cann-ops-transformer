deb550938e7dfd2e11dcff38e99842d77cfdff3c
Ahmet Yuzuguler <ahmet.yuzuguler@huawei.com>
2025-12-12 14:17:43 +0000

    Add more loops to the perf bench

--- experimental/attention/typhoon_mla/bench.py
+++ experimental/attention/typhoon_mla/bench.py
@@ -13,31 +13,30 @@ from tests.utils import convert_absorb_to_naive, kv_to_paged, convert_to_dense
 torch.set_printoptions(sci_mode=False)
 
 if __name__=="__main__":
-    n_heads = 128
-    n_kv_heads = 128
     qk_nope_head_dim = 128
     qk_rope_head_dim = 64
     kv_lora_rank = 512
     v_head_dim = 128
     softmax_scale = 1/np.sqrt(128)
+    block_size = 128
 
     device = torch.device("npu:0")
     dtype = torch.bfloat16
 
-    for bsz in [64, 128, 256, 512]:
-        shared_seqlen = 4096
-        nonshared_seqlen = 128
+    for n_heads in [64, 128]:
+        for bsz in [128, 256, 512]:
+            for shared_seqlen in [4*1024, 8*1024, 16*1024]:
+                for nonshared_seqlen in [256, 1024, 4096]:
+                    seqlens = [shared_seqlen] + [nonshared_seqlen] * bsz
+                    
+                    test_typhoon_mla = TyphoonMLA(bsz, seqlens, n_heads, qk_nope_head_dim, qk_rope_head_dim, kv_lora_rank, v_head_dim, softmax_scale, device, dtype)
+                    typhoonmla_elapsed = test_typhoon_mla.perf(warm_up=25, n_repeat=100)
+                    typhoonmla_tgr = bsz/(typhoonmla_elapsed*1e-3)*1e-3 #ktoken/s
 
-        block_size = 128
-        seqlens = [shared_seqlen] + [nonshared_seqlen] * bsz
-        
-        test_typhoon_mla = TyphoonMLA(bsz, seqlens, n_heads, qk_nope_head_dim, qk_rope_head_dim, kv_lora_rank, v_head_dim, softmax_scale, device, dtype)
-        typhoonmla_elapsed = test_typhoon_mla.perf(warm_up=25, n_repeat=100)
-        typhoonmla_tgr = bsz/(typhoonmla_elapsed*1e-3)*1e-3 #ktoken/s
+                    seqlens = [shared_seqlen + nonshared_seqlen] * bsz
+                    test_torchnpu_paged_mla = TorchNPUPagedMLA(bsz, seqlens, n_heads, qk_nope_head_dim, qk_rope_head_dim, kv_lora_rank, v_head_dim, softmax_scale, device, dtype)
+                    torchnpu_elapsed = test_torchnpu_paged_mla.perf(warm_up=25, n_repeat=100)
+                    torchnpu_tgr = bsz/(torchnpu_elapsed*1e-3)*1e-3 #ktoken/s
 
-        seqlens = [shared_seqlen + nonshared_seqlen] * bsz
-        test_torchnpu_paged_mla = TorchNPUPagedMLA(bsz, seqlens, n_heads, qk_nope_head_dim, qk_rope_head_dim, kv_lora_rank, v_head_dim, softmax_scale, device, dtype)
-        torchnpu_elapsed = test_torchnpu_paged_mla.perf(warm_up=25, n_repeat=100)
-        torchnpu_tgr = bsz/(torchnpu_elapsed*1e-3)*1e-3 #ktoken/s
-        
-        print(f"bsz: {bsz:<5} shared_kv_seqlen: {shared_seqlen:<5} nonshared_kv_seqlen: {nonshared_seqlen:<5} | TyphoonMLA (TBT): {typhoonmla_elapsed:.2f} ms   TorchNPU-Absorb (TBT): {torchnpu_elapsed:.2f} ms")
+                    speedup = typhoonmla_tgr / torchnpu_tgr
+                    print(f"batch: {bsz:<5}  shared_seqlen: {shared_seqlen:<5}  nonshared_seqlen: {nonshared_seqlen:<5}  headnum: {n_heads:<5}  |  TyphoonMLA: {typhoonmla_tgr:>6.2f} ktoken/s  TorchNPU-Absorb: {torchnpu_tgr:>6.2f} ktoken/s  Speedup: {speedup:.2f}x")
