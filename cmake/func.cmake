4a8dc4cae7887e4c82f2067e3ac9c5f12cc31fc5
xutianze <xutianze2@huawei.com>
2025-12-17 19:38:54 +0800

    mc2新特性开发

--- cmake/func.cmake
+++ cmake/func.cmake
@@ -344,6 +344,7 @@ function(add_ops_src_copy)
     endif ()
 
     set(MC2_OPS_LIST "matmul_reduce_scatter;"
+        "matmul_reduce_scatter_v2;"
         "grouped_mat_mul_allto_allv;"
         "batch_mat_mul_reduce_scatter_allto_all;"
         "allto_allv_grouped_mat_mul;"
@@ -356,6 +357,7 @@ function(add_ops_src_copy)
         "moe_distribute_combine_v2;"
         "moe_update_expert;"
         "all_gather_matmul;"
+        "all_gather_matmul_v2;"
         "matmul_all_reduce;"
         "matmul_all_reduce_add_rms_norm;"
         "inplace_matmul_all_reduce_add_rms_norm;") # mc2算子列表
--- mc2/3rd/weight_quant_batch_matmul_v2/op_kernel/fixpipe/weight_quant_batch_matmul_v2_fixpipe_stage2.h
+++ mc2/3rd/weight_quant_batch_matmul_v2/op_kernel/fixpipe/weight_quant_batch_matmul_v2_fixpipe_stage2.h
@@ -1,6 +1,6 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -19,17 +19,17 @@
 #include "kernel_operator_intf.h"
 #include "../tool.h"
 
-namespace WeightQuantBatchMatmulV2 {
+namespace Mc2WeightQuantBatchMatmulV2 {
 using AscendC::BLOCK_CUBE;
 using AscendC::FixpipeParamsV220;
 using AscendC::LoadData2DParams;
 
 // a * weight_fp16 + bias_fp16 = y_fp16
 template <bool hasBias>
-class WeightQuantBatchMatmulV2FixpipeStage2
+class Mc2WeightQuantBatchMatmulV2FixpipeStage2
 {
 public:
-    __aicore__ inline WeightQuantBatchMatmulV2FixpipeStage2(){};
+    __aicore__ inline Mc2WeightQuantBatchMatmulV2FixpipeStage2(){};
     __aicore__ inline void Init(
         const LocalTensor<half>& a16L0A, const LocalTensor<half>& w16L0B, const LocalTensor<float>& biasBT,
         const LocalTensor<float>& y32C01, const GlobalTensor<half>& yGm)
@@ -194,5 +194,5 @@ private:
     uint32_t nProcessFracBlk_; // n维度分形数量，n0LenAlign/16
     uint64_t pingPongOffset_ = 0;
 };
-} // namespace WeightQuantBatchMatmulV2
+} // namespace Mc2WeightQuantBatchMatmulV2
 #endif // WEIGHT_QUANT_BATCH_MATMUL_V2_FIXPIPE_STAGE2_H
