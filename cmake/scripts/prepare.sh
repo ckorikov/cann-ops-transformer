5661e4e52e74aab89b6152f0ec43481a93bb6bc8
lidongsheng <lidongsheng43@huawei.com>
2025-12-18 14:37:12 +0800

    experimental编包验证适配

--- cmake/scripts/prepare.sh
+++ cmake/scripts/prepare.sh
@@ -81,6 +81,10 @@ while [[ $# -gt 0 ]]; do
         ENABLE_BUILT_IN="$2"
         shift 2
         ;;
+    --enable_experimental)
+        ENABLE_EXPERIMENTAL="$2"
+        shift 2
+        ;;
     --enable_ccache)
         ENABLE_CCACHE="$2"
         shift 2
@@ -133,6 +137,7 @@ function build() {
         -DENABLE_CCACHE=${ENABLE_CCACHE} \
         -DBUILD_OPS_RTY_KERNEL=${BUILD_OPS_RTY_KERNEL} \
         -DENABLE_BUILT_IN=${ENABLE_BUILT_IN} \
+        -DENABLE_EXPERIMENTAL=${ENABLE_EXPERIMENTAL} \
         -DOP_DEBUG_CONFIG=${OP_DEBUG_CONFIG}
 
     make ${JOB_NUM} prepare_build
