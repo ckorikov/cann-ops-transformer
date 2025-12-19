8fd266e64b1dd6dc307b948a39c70db548348490
xutianze <xutianze2@huawei.com>
2025-12-19 20:32:08 +0800

    mc2新特性开发

--- cmake/func.cmake
+++ cmake/func.cmake
@@ -389,12 +389,6 @@ function(add_ops_src_copy)
         "matmul_all_reduce;"
         "matmul_all_reduce_add_rms_norm;"
         "inplace_matmul_all_reduce_add_rms_norm;"
-        "quant_reduce_scatter;"
-        "quant_all_reduce;"
-        "matmul_allto_all;"
-        "allto_all_matmul;"
-        "attention_to_ffn;"
-        "ffn_to_attention;"
     ) # mc2算子列表
 
     get_filename_component(FOLDER_NAME "${SRC_COPY_DST}" NAME_WE)
