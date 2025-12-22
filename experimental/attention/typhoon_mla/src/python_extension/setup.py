60b7e16e8d29a3ff68171a1a01899250acdcb316
Ahmet Yuzuguler <ahmet.yuzuguler@huawei.com>
2025-12-22 15:08:39 +0000

    Fix license headers

--- experimental/attention/typhoon_mla/src/python_extension/setup.py
+++ experimental/attention/typhoon_mla/src/python_extension/setup.py
@@ -1,3 +1,13 @@
+# -----------------------------------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# -----------------------------------------------------------------------------------------------------------
+
 import logging
 import os
 import subprocess as sp
@@ -36,7 +46,7 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        # Use subprocess to invoke CMake for configuring and building the extension.
+        # Use sp to invoke CMake for configuring and building the extension.
         # This executes external build commands in a controlled build directory.
         sp.check_call(["cmake", ext.sourcedir] +
                               cmake_args, cwd=self.build_temp)
