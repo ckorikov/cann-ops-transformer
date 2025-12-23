555d7ae6a520b8d70947fbb998df53afa13ac77f
Zhen Wang <wangzhen407@huawei.com>
2025-12-23 15:01:48 +0800

    [MlaProlog] Fix preicision issue when queryNormFlag is set to true

--- attention/mla_prolog/op_kernel/kernel_mla_prolog_split_n.h
+++ attention/mla_prolog/op_kernel/kernel_mla_prolog_split_n.h
@@ -624,14 +624,19 @@ __aicore__ inline void MlaPrologVecS1CubS2<MLAPT>::WorkspaceInit(__gm__ uint8_t 
                        static_cast<int64_t>(sizeof(mmCkvKrOutputType));
 
     mmCqResGm_.SetGlobalBuffer((__gm__ mmCqOutputType *)(workspace + workspaceOffset));
-    if constexpr (!std::is_same<rmsNormCqOutputType, mmCqOutputType>::value) {
-        workspaceOffset += static_cast<int64_t>(baseParams_->stepBatchSize) * static_cast<int64_t>(baseParams_->headSizeCq) * static_cast<int64_t>(sizeof(mmCqOutputType));
-    }
-
+    
     if (baseParams_->queryNormFlag == 0U) {
+        // 全量化场景下`mmCqResGm_`与`rmsNormCqResGm_`无法共用workspace，其他场景下两者共用workspace
+        if constexpr (!std::is_same<rmsNormCqOutputType, mmCqOutputType>::value) {
+            workspaceOffset += static_cast<int64_t>(baseParams_->stepBatchSize) * static_cast<int64_t>(baseParams_->headSizeCq) * static_cast<int64_t>(sizeof(mmCqOutputType));
+        }
+        // 不返回queryNorm时，rmsNormCq结果放在workspace
         rmsNormCqResGm_.SetGlobalBuffer((__gm__ rmsNormCqOutputType *)(workspace + workspaceOffset));
         workspaceOffset += static_cast<int64_t>(baseParams_->stepBatchSize) *
                            static_cast<int64_t>(baseParams_->headSizeCq) * sizeof(rmsNormCqOutputType);
+    } else {
+        // 返回queryNorm时，rmsNormCq结果直接输出gm，此处需要偏移`mmCqResGm_`占用的大小
+        workspaceOffset += static_cast<int64_t>(baseParams_->stepBatchSize) * static_cast<int64_t>(baseParams_->headSizeCq) * static_cast<int64_t>(sizeof(mmCqOutputType));
     }
 
     mmQcQrResGm_.SetGlobalBuffer((__gm__ mmQcQrOutputType *)(workspace + workspaceOffset));
