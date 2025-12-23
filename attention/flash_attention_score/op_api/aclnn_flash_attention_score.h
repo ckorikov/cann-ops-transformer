8e10813b21755db1a5e864fe975dbbba9ae93925
huanyongben <huanyongben1@hisilicon.com>
2025-12-23 22:21:01 +0800

    add 950 FA/FAG

--- attention/flash_attention_score/op_api/aclnn_flash_attention_score.h
+++ attention/flash_attention_score/op_api/aclnn_flash_attention_score.h
@@ -1,12 +1,12 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #ifndef OP_API_INC_LEVEL2_ACLNN_FLASH_ATTENTION_SCORE_H_
 #define OP_API_INC_LEVEL2_ACLNN_FLASH_ATTENTION_SCORE_H_
@@ -131,6 +131,58 @@ aclnnStatus aclnnFlashAttentionScoreV3GetWorkspaceSize(
  * @brief aclnnFlashAttentionScoreV3的第二段接口，用于执行计算。
 */
 aclnnStatus aclnnFlashAttentionScoreV3(
+    void *workspace,
+    uint64_t workspaceSize,
+    aclOpExecutor *executor,
+    const aclrtStream stream);
+
+/**
+ * @brief aclnnFlashAttentionScoreV4的第一段接口，根据具体的计算流程，计算workspace大小。
+ * @domain aclnn_ops_train
+ */
+aclnnStatus aclnnFlashAttentionScoreV4GetWorkspaceSize(
+    const aclTensor *query,
+    const aclTensor *key,
+    const aclTensor *value,
+    const aclTensor *realShiftOptional,
+    const aclTensor *dropMaskOptional,
+    const aclTensor *paddingMaskOptional,
+    const aclTensor *attenMaskOptional,
+    const aclTensor *queryRopeOptional,
+    const aclTensor *keyRopeOptional,
+    const aclTensor *dScaleQOptional,
+    const aclTensor *dScaleKOptional,
+    const aclTensor *dScaleVOptional,
+    const aclTensor *sinkOptional,
+    const aclIntArray *prefixOptional,
+    const aclIntArray *actualSeqQLenOptional, /*varlen only*/
+    const aclIntArray *actualSeqKvLenOptional, /*varlen only*/
+    const aclIntArray *qStartIdxOptional,
+    const aclIntArray *kvStartIdxOptional,
+    double scaleValue,
+    double keepProb,
+    int64_t preTokens,
+    int64_t nextTokens,
+    int64_t headNum,
+    char *inputLayout,
+    int64_t innerPrecise,
+    int64_t sparseMode,
+    int64_t outDtype,
+    int64_t pseType,
+    char *softmaxOutLayout,
+    int64_t seed, /*dropout ADD*/
+    int64_t offset, /*dropout ADD*/
+    const aclTensor *softmaxMaxOut,
+    const aclTensor *softmaxSumOut,
+    const aclTensor *softmaxOutOut,
+    const aclTensor *attentionOutOut,
+    uint64_t *workspaceSize,
+    aclOpExecutor **executor);
+
+/**
+ * @brief aclnnFlashAttentionScoreV4的第二段接口，用于执行计算。
+ */
+aclnnStatus aclnnFlashAttentionScoreV4(
     void *workspace,
     uint64_t workspaceSize,
     aclOpExecutor *executor,
--- attention/flash_attention_score_grad/examples/test_aclnn_flash_attention_unpadding_score_grad.cpp
+++ attention/flash_attention_score_grad/examples/test_aclnn_flash_attention_unpadding_score_grad.cpp
@@ -1,12 +1,12 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
- * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
- */
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 /*!
  * \file test_aclnn_flash_attention_score_grad.cpp
@@ -200,7 +200,7 @@ int main() {
   int64_t headNum = 1;
   int64_t innerPrecise = 0;
   int64_t sparseMode = 0;
-  char softmaxInLayoutArr[] = "same_as_input";
+  char softmaxInLayoutArr[] = "";
 
   char layOut[5] = {'T', 'N', 'D', 0};
 
