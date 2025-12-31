17b42e8eefe412071582adb7c71e83867d1fb20a
s00870334 <sangzhenguo@huawei.com>
2025-12-31 11:48:47 +0800

    bug fix

--- examples/mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
+++ examples/mc2/all_gather_add/examples/test_aclnn_all_gather_add.cpp
@@ -204,7 +204,6 @@ int LaunchOneThreadAllGatherAdd(Args &args, TestData &testData)
 
     auto hcclRet = HcclCommDestroy(args.hcclComm);
     CHECK_RET(hcclRet == HCCL_SUCCESS, LOG_PRINT("[ERROR] HcclCommDestroy failed. ret = %d \n", hcclRet));
-    LOG_PRINT("[INFO] device_%d aclnnAllGatherAdd HcclCommDestroy successfully.\n", args.rankId);
     // 释放device资源，需要根据具体API的接口定义修改
     if (a != nullptr) {
         aclDestroyTensor(a);
