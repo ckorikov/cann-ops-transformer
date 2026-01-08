f8f44e9507d711afb1b7a2935bc92bb113369c36
smdbha <panzhijie2@huawei.com>
2026-01-08 11:41:12 +0800

    GMM公共模板代码整改

--- gmm/grouped_matmul_finalize_routing/op_kernel/arch35/grouped_matmul_finalize_routing.h
+++ gmm/grouped_matmul_finalize_routing/op_kernel/arch35/grouped_matmul_finalize_routing.h
@@ -16,9 +16,9 @@
 #ifndef GROUPED_MATMUL_FINALIZE_ROUTING_H
 #define GROUPED_MATMUL_FINALIZE_ROUTING_H
 
-#include "../../../common/groupedmatmul_act/kernel/kernel_gmm_finalize_routing.h"
-#include "../../../common/groupedmatmul_act/block/block_mx_mm_aic_to_aiv_builder.h"
-#include "../../../common/groupedmatmul_act/block/block_scheduler_gmm_aswt_with_tail_split.h"
+#include "../../../common/cgmct/kernel/kernel_gmm_finalize_routing.h"
+#include "../../../common/cgmct/block/block_mx_mm_aic_to_aiv_builder.h"
+#include "../../../common/cgmct/block/block_scheduler_gmm_aswt_with_tail_split.h"
 #include "grouped_matmul_finalize_routing_tiling_data.h"
 
 using namespace Act::Gemm;
