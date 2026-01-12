9fcfb295931331fd61b0048d58b47f08391a553e
INeedAnID <panjie38@huawei.com>
2026-01-12 10:36:14 +0800

    sinks

--- experimental/attention/sparse_attn_sharedkv/op_host/sparse_attn_sharedkv_tiling.h
+++ experimental/attention/sparse_attn_sharedkv/op_host/sparse_attn_sharedkv_tiling.h
@@ -100,6 +100,7 @@ constexpr uint32_t DIM_IDX_THREE = 3;
 constexpr uint32_t DIM_IDX_FOUR = 4;
 
 // Dim Num
+constexpr uint32_t DIM_NUM_ONE = 1;
 constexpr uint32_t DIM_NUM_TWO = 2;
 constexpr uint32_t DIM_NUM_THREE = 3;
 constexpr uint32_t DIM_NUM_FOUR = 4;
@@ -437,6 +438,7 @@ public:
     ge::graphStatus GetSparseBlockCount();
     ge::graphStatus GetActualseqInfo();
     ge::graphStatus GetSASTempateMode();
+    ge::graphStatus GetSinks();
     void GenerateInfo(SASTilingInfo &sasInfo);
     ge::graphStatus Parse(SASTilingInfo &sasInfo);
 
